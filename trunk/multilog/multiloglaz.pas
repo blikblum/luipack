e2c90bc4cd9b61238723a5cacd1ee746cbcaee21
blikblum <blikblum@d0c4a878-0f54-0410-8585-a5e1b55c9de8>
2007-02-02 19:31:21 +0000

    multilog - initial import

---
+++ trunk/multilog/multiloglaz.pas
@@ -0,0 +1,21 @@
+{ This file was automatically created by Lazarus. Do not edit!
+This source is only used to compile and install the package.
+ }
+
+unit multiloglaz; 
+
+interface
+
+uses
+  registermultilog, LazarusPackageIntf; 
+
+implementation
+
+procedure Register; 
+begin
+  RegisterUnit('registermultilog', @registermultilog.Register); 
+end; 
+
+initialization
+  RegisterPackage('multiloglaz', @Register); 
+end.
