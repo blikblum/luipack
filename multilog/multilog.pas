ef1174e16c92dbf3c2ed439ee3299c030962760f
blikblum <blikblum@d0c4a878-0f54-0410-8585-a5e1b55c9de8>
2007-02-11 14:25:00 +0000

    Multilog - polished the demo app and multilogviewer

--- multilog/multilog.pas
+++ multilog/multilog.pas
@@ -1,11 +1,26 @@
 unit multilog;
 
-{ Copyright (C) 2006 Luiz Américo Pereira Câmara
+{
+  Main unit of the Multilog logging system
+
+  Copyright (C) 2006 Luiz Américo Pereira Câmara
+  pascalive@bol.com.br
 
   This library is free software; you can redistribute it and/or modify it
   under the terms of the GNU Library General Public License as published by
   the Free Software Foundation; either version 2 of the License, or (at your
-  option) any later version.
+  option) any later version with the following modification:
+
+  As a special exception, the copyright holders of this library give you
+  permission to link this library with independent modules to produce an
+  executable, regardless of the license terms of these independent modules,and
+  to copy and distribute the resulting executable under terms of your choice,
+  provided that you also meet, for each linked independent module, the terms
+  and conditions of the license of that module. An independent module is a
+  module which is not derived from or based on this library. If you modify
+  this library, you may extend this exception to your version of the library,
+  but you are not obligated to do so. If you do not wish to do so, delete this
+  exception statement from your version.
 
   This program is distributed in the hope that it will be useful, but WITHOUT
   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -16,6 +31,7 @@ unit multilog;
   along with this library; if not, write to the Free Software Foundation,
   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 }
+
 
 {$mode objfpc}{$H+}
 
