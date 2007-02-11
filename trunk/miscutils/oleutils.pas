de26fa53ad1dcd177e2981f2a1f47fc9efd55120
blikblum <blikblum@d0c4a878-0f54-0410-8585-a5e1b55c9de8>
2007-02-11 16:28:42 +0000

    MiscUtils - finished TOleStream

--- trunk/miscutils/oleutils.pas
+++ trunk/miscutils/oleutils.pas
@@ -29,7 +29,7 @@ unit oleutils;
 interface
 {$ifdef Windows}
 uses
-  Windows, Classes, ActiveX;
+  Windows, Classes, SysUtils, ActiveX;
 
 type
 
@@ -38,15 +38,37 @@ type
   TOLEStream = class (TStream)
   private
     FSrcStream: IStream;
+    procedure InternalSetSize(NewSize: LARGE_INTEGER);
   public
     constructor Create(const Stream: IStream);
     function Read(var Buffer; Count: Integer): Integer; override;
     function Seek(Offset: Integer; Origin: Word): Integer; overload; override;
+    procedure SetSize(const NewSize: Int64); override;
+    procedure SetSize(NewSize: Longint); override;
     function Write(const Buffer; Count: Integer): Integer; override;
   end;
 {$endif}
 implementation
 {$ifdef Windows}
+
+function ErrorString(Error: HRESULT): String;
+begin
+  case Error of
+    E_PENDING: Result:='E_PENDING';
+    S_FALSE: Result:='S_FALSE';
+    STG_E_MEDIUMFULL: Result:='STG_E_MEDIUMFULL';
+    STG_E_ACCESSDENIED: Result:= 'STG_E_ACCESSDENIED';
+    STG_E_CANTSAVE: Result:='STG_E_CANTSAVE';
+    STG_E_INVALIDPOINTER: Result:='STG_E_INVALIDPOINTER';
+    STG_E_REVERTED: Result:='STG_E_REVERTED';
+    STG_E_WRITEFAULT: Result:='STG_E_WRITEFAULT';
+    STG_E_INVALIDFUNCTION: Result:='STG_E_INVALIDFUNCTION';
+ else
+   Result:='Unknow error';
+ end;
+  
+end;
+
 { TOLEStream }
 
 constructor TOLEStream.Create(const Stream: IStream);
@@ -56,24 +78,61 @@ begin
 end;
 
 function TOLEStream.Read(var Buffer; Count: Integer): Integer;
+var
+  Res: HRESULT;
 begin
-  FSrcStream.Read(@Buffer, Count, @Result);
+  Res:=FSrcStream.Read(@Buffer, Count, @Result);
+  if Res <> S_OK then
+    Raise Exception.Create('TOLEStream - Error while reading: '+ErrorString(Res));
 end;
 
 function TOLEStream.Seek(Offset: Integer; Origin: Word): Integer;
 var
   liResult, liOffset : LARGE_INTEGER;
+  Res: HRESULT;
 begin
   //soFrom* constants are equal to STREAM_SEEK_* constants. Assume it here
   liOffset.LowPart:=Offset;
   liOffset.HighPart:=0;
-  FSrcStream.Seek(liOffset, Origin, liResult);
+  Res:=FSrcStream.Seek(liOffset, Origin, liResult);
   Result:=liResult.LowPart;
+  if Res <> S_OK then
+    Raise Exception.Create('TOLEStream - Error while seeking: '+ErrorString(Res));
+end;
+
+procedure TOLEStream.SetSize(NewSize: Longint);
+var
+  liSize: LARGE_INTEGER;
+begin
+  liSize.LowPart:=NewSize;
+  liSize.HighPart:=0;
+  InternalSetSize(liSize);
+end;
+
+procedure TOLEStream.SetSize(const NewSize: Int64);
+var
+  liSize: LARGE_INTEGER;
+begin
+  liSize.QuadPart:=NewSize;
+  InternalSetSize(liSize);
+end;
+
+procedure TOLEStream.InternalSetSize(NewSize: LARGE_INTEGER);
+var
+  Res:HRESULT;
+begin
+  Res:=FSrcStream.SetSize(NewSize);
+  if Res <> S_OK then
+    Raise Exception.Create('TOLEStream - Error while setting size: '+ErrorString(Res));
 end;
 
 function TOLEStream.Write(const Buffer; Count: Integer): Integer;
+var
+  Res: HRESULT;
 begin
-  FSrcStream.Write(@Buffer,Count,@Result);
+  Res:=FSrcStream.Write(@Buffer,Count,@Result);
+  if Res <> S_OK then
+    Raise Exception.Create('TOLEStream - Error while writing: '+ErrorString(Res));
 end;
 {$endif}
 end.
