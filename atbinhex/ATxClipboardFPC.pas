93a6879d32d1af68ebf27fd09357073966e0dfc7
blikblum <blikblum@d0c4a878-0f54-0410-8585-a5e1b55c9de8>
2007-04-04 18:14:11 +0000

    ATBinHex - Implemented clipboard support

--- atbinhex/ATxClipboardFPC.pas
+++ atbinhex/ATxClipboardFPC.pas
@@ -11,7 +11,7 @@ LCL port: Luiz Americo Pereira Camara
 interface
 
 uses
-  LCLIntf, Classes, SysUtils, Clipbrd;
+  lclext, DelphiCompat, LCLIntf, Classes, SysUtils, Clipbrd;
 
 function SClearClipboard: boolean;
 function SCopyToClipboard(const S: AnsiString; IsOEM: boolean = false): boolean;
@@ -21,99 +21,37 @@ function SCopyToClipboardW(const S: WideString): boolean;
 implementation
 
 function SClearClipboard: boolean;
-//todo: see if is necessary call OpenClipboard before calling Empty clipboard and fix LCL
 begin
-  //if not OpenClipboard(0) then begin Result:= false; Exit end;
-  //Result:=EmptyClipboard;
+  //not necessary in LCL
+  //Result:= EmptyClipboard;
 end;
 
-//CF_UNICODETEXT format is supported only under NT
 function SCopyToClipboardW_NT(const S: WideString; DoClear: boolean): boolean;
 var
-  DataSize, BufferSize: integer;
-  hData: HGLOBAL;
-  gData: pointer;
+  DataSize: integer;
 begin
-//todo: implement to LCL
-{  DataSize:= Length(S)*2;
-  if DataSize>0 then
-    begin
-    if DoClear then
-      begin
-      if not OpenClipboard(0) then begin Result:= false; Exit end;
-      EmptyClipboard;
-      end;
-
-    BufferSize:= DataSize+2;
-    hData:= GlobalAlloc(GMEM_MOVEABLE or GMEM_DDESHARE, BufferSize);
-    if hData<>0 then
-      begin
-      gData:= GlobalLock(hData);
-      if gData<>nil then
-        begin
-        Move(S[1], gData^, BufferSize);
-        GlobalUnlock(hData);
-        SetClipboardData(CF_UNICODETEXT, hData);
-        end;
-      end;
-
-    if DoClear then
-      CloseClipboard;
-    end;
-  Result:= true;
-}
+  DataSize:= Length(S)*2 + 1;
+  if DoClear then
+    Clipboard.Clear;
+  //CF_UNICODETEXT returns Windows.CF_UNICODETEXT
+  Clipboard.AddFormat(CF_UNICODETEXT,PWideChar(S)^,DataSize);
+  Result:= True;
 end;
 
 function SCopyToClipboard(const S: AnsiString; IsOEM: boolean = false): boolean;
-//const
-  //Formats: array[boolean] of integer = (CF_TEXT, CF_OEMTEXT);
-var
-
-  DataSize, BufferSize: integer;
-//  hData: HGLOBAL;
-  gData: pointer;
 begin
-  //todo: ImplementeLCL
-  {
-  DataSize:= Length(S);
-  if DataSize>0 then
-    begin
-    if not OpenClipboard(0) then begin Result:= false; Exit end;
-    EmptyClipboard;
-
-    BufferSize:= DataSize+1;
-    hData:= GlobalAlloc(GMEM_MOVEABLE or GMEM_DDESHARE, BufferSize);
-    if hData<>0 then
-      begin
-      gData:= GlobalLock(hData);
-      if gData<>nil then
-        begin
-        Move(S[1], gData^, BufferSize);
-        GlobalUnlock(hData);
-        SetClipboardData(Formats[IsOEM], hData);
-        end;
-      end;
-
-    //Also copy in CF_UNICODETEXT format for compatability with Windows controls
-    if Win32Platform=VER_PLATFORM_WIN32_NT then
-      if IsOEM
-        then SCopyToClipboardW_NT(WideString(ToANSI(S)), false)
-        else SCopyToClipboardW_NT(WideString(S), false);
-
-    CloseClipboard;
-    end;
-
-  Result:= true;
-  }
+  //todo: Implement OEM support
+  Clipboard.AsText:=S;
+  Result:=True;
 end;
 
 
 function SCopyToClipboardW(const S: WideString): boolean;
 begin
-  //todo
-  if True
-    then Result:= SCopyToClipboardW_NT(S, true)
-    else Result:= SCopyToClipboard(AnsiString(S));;
+  if OSSupportsUTF16 then
+    Result:= SCopyToClipboardW_NT(S, True)
+  else
+    Result:= SCopyToClipboard(UTF8Encode(S));
 end;
 
 
--- atbinhex/lcldemo/atbinhexdemo.lpr
+++ atbinhex/lcldemo/atbinhexdemo.lpr
@@ -8,7 +8,7 @@ uses
   {$ENDIF}{$ENDIF}
   Interfaces, // this includes the LCL widgetset
   Forms
-  { add your units here }, fmain, atbinhex_lcl;
+  { add your units here }, fmain;
 
 begin
   Application.Title:='ATBinHexLCLDemo';
