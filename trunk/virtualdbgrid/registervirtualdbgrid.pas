2db3d3f7dd12c0624666165c9391b0782fc820f8
blikblum <blikblum@d0c4a878-0f54-0410-8585-a5e1b55c9de8>
2007-08-05 02:07:28 +0000

    * VirtualDbGrid: Added package files

---
+++ trunk/virtualdbgrid/registervirtualdbgrid.pas
@@ -0,0 +1,30 @@
+unit registervirtualdbgrid; 
+
+{$Mode ObjFpc}
+{$H+}
+
+interface
+
+uses 
+  Classes, SysUtils, LResources, LazarusPackageIntf,
+  VirtualDBGrid;
+  
+procedure Register;
+
+implementation
+
+procedure RegisterUnitVirtualDbGrid;
+begin
+  RegisterComponents('Virtual Treeview', [TVirtualDbGrid]);
+end;  
+
+procedure Register;
+
+begin
+  RegisterUnit('VirtualDbGrid',@RegisterUnitVirtualDbGrid);
+end; 
+
+initialization
+{.$i ideicons.lrs}
+ 
+end.
