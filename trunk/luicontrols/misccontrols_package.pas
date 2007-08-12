32c325bf273ecd7bba441bd9041c8e1a76ee5674
blikblum <blikblum@d0c4a878-0f54-0410-8585-a5e1b55c9de8>
2007-08-12 18:17:33 +0000

    * Renamed MiscControls to LuiControls

---
+++ trunk/luicontrols/misccontrols_package.pas
@@ -0,0 +1,21 @@
+{ This file was automatically created by Lazarus. Do not edit!
+This source is only used to compile and install the package.
+ }
+
+unit misccontrols_package; 
+
+interface
+
+uses
+  ToggleLabel, registermisccontrols, MenuButton, LazarusPackageIntf; 
+
+implementation
+
+procedure Register; 
+begin
+  RegisterUnit('registermisccontrols', @registermisccontrols.Register); 
+end; 
+
+initialization
+  RegisterPackage('misccontrols_package', @Register); 
+end.
