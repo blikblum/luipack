98e94050991747118ea4b40cdb7670ba10c10e16
blikblum <blikblum@d0c4a878-0f54-0410-8585-a5e1b55c9de8>
2007-09-13 00:25:15 +0000

    * UniqueInstance:   - Fixed compilation under Linux   - Initialized UpdateInterval by hand   - Added button to test after a app crash   - The component is placed is system tab

--- uniqueinstance/testcomponent/ftesform.pas
+++ uniqueinstance/testcomponent/ftesform.pas
@@ -13,9 +13,11 @@ type
   { TForm1 }
 
   TForm1 = class(TForm)
+    ButCrashApp: TButton;
     Label1: TLabel;
     ListBox1: TListBox;
     UniqueInstance1: TUniqueInstance;
+    procedure ButCrashAppClick(Sender: TObject);
     procedure UniqueInstance1OtherInstance(Sender: TObject; Count: Integer;
       Parameters: array of String);
   private
@@ -41,6 +43,15 @@ begin
   for i := 0 to Count - 1 do
     ListBox1.Items.Add(Parameters[i]);
   BringToFront;
+end;
+
+procedure TForm1.ButCrashAppClick(Sender: TObject);
+var
+  d: Double;
+  x: integer;
+begin
+  x := 0;
+  d := 1 / x;
 end;
 
 initialization
