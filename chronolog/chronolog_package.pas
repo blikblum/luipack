65a69cde4453f2aeeef94d70cbc22511fc12886c
blikblum <blikblum@d0c4a878-0f54-0410-8585-a5e1b55c9de8>
2007-10-28 00:28:57 +0000

    * Chronolog:  - added missing package files

---
+++ chronolog/chronolog_package.pas
@@ -0,0 +1,20 @@
+{ This file was automatically created by Lazarus. Do not edit!
+This source is only used to compile and install the package.
+ }
+
+unit chronolog_package; 
+
+interface
+
+uses
+  chronolog, chrono2db, LazarusPackageIntf; 
+
+implementation
+
+procedure Register; 
+begin
+end; 
+
+initialization
+  RegisterPackage('chronolog_package', @Register); 
+end.
