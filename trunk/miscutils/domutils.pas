aeb13fbe656d3dacf8954c6d092cf637ff59a55a
blikblum <blikblum@d0c4a878-0f54-0410-8585-a5e1b55c9de8>
2008-02-10 15:18:27 +0000

    * MiscUtils  - Add license information  - Updated package description

--- trunk/miscutils/domutils.pas
+++ trunk/miscutils/domutils.pas
@@ -1,34 +1,47 @@
-unit domutils;
+unit DomUtils;
 
- { Auxiliary DOM classes
+ { Helper DOM classes
 
   Copyright (C) 2007 Luiz Américo Pereira Câmara
   pascalive@bol.com.br
 
-  This source is free software; you can redistribute it and/or modify it under
-  the terms of the GNU General Public License as published by the Free
-  Software Foundation; either version 2 of the License, or (at your option)
-  any later version.
 
-  This code is distributed in the hope that it will be useful, but WITHOUT ANY
-  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-  details.
+  This library is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Library General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or (at your
+  option) any later version with the following modification:
 
-  A copy of the GNU General Public License is available on the World Wide Web
-  at <http://www.gnu.org/copyleft/gpl.html>. You can also obtain it by writing
-  to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
-  MA 02111-1307, USA.
+  As a special exception, the copyright holders of this library give you
+  permission to link this library with independent modules to produce an
+  executable, regardless of the license terms of these independent modules,and
+  to copy and distribute the resulting executable under terms of your choice,
+  provided that you also meet, for each linked independent module, the terms
+  and conditions of the license of that module. An independent module is a
+  module which is not derived from or based on this library. If you modify
+  this library, you may extend this exception to your version of the library,
+  but you are not obligated to do so. If you do not wish to do so, delete this
+  exception statement from your version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Library General Public License
+  for more details.
+
+  You should have received a copy of the GNU Library General Public License
+  along with this library; if not, write to the Free Software Foundation,
+  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 }
 
 {$mode objfpc}
 {$H+}
 
 interface
+
 uses
-  Dom,Classes,DB,miscutils;
+  Dom, Classes, DB, miscutils;
 
 type
+
   THtmlSubTree = class;
 
   { THtmlTree }
@@ -110,7 +123,8 @@ type
   end;
 
 
-  TColumnCallback = function:string;
+  TColumnCallback = function: String;
+
   TDBTree = class;
   { TDbColumn }
 
@@ -172,7 +186,7 @@ type
 implementation
 
 uses
-  SysUtils,XmlWrite;
+  SysUtils, XmlWrite;
 
 constructor THtmlTree.Create;
 begin
