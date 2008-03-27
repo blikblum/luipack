a3b5355ce49d5411b7a6bd6bc1abb720c166cb5b
blikblum <blikblum@d0c4a878-0f54-0410-8585-a5e1b55c9de8>
2008-03-27 02:17:38 +0000

    * CairoUtils:  - Added functions to retrieve coordinates for a sharp line stroke

--- cairo/classes/cairoutils.pas
+++ cairo/classes/cairoutils.pas
@@ -7,18 +7,73 @@ interface
 uses
   Classes, SysUtils, CairoClasses, math;
 
+type
+
+  TDoubleRect = record
+    Left: Double;
+    Top: Double;
+    Right: Double;
+    Bottom: Double;
+  end;
+
 function DegreeToRadian(const Angle: Double): Double;
+
+procedure CalculateSharpBounds(ALeft, ATop, ARight, ABottom, LineWidth: Integer; out Bounds: TDoubleRect);
+
+procedure CalculateSharpRect(X, Y, Width, Height, LineWidth: Integer; out Rect: TDoubleRect);
+
+function GetSharpLineOffset(LineWidth: Integer): Double;
 
 function RadianToDegree(const Angle: Double): Double;
 
 procedure RoundedRectangle(Context: TCairoContext; X, Y, Width, Height: Double;
   TopRadius: Double; BottomRadius: Double = -1);
 
+
+
 implementation
 
 function DegreeToRadian(const Angle: Double): Double;
 begin
   Result := Angle * (PI / 180);
+end;
+
+function GetSharpLineOffset(LineWidth: Integer): Double;
+begin
+  if Odd(LineWidth) then
+    Result := (LineWidth div 2) + 0.5
+  else
+    Result := LineWidth / 2;
+end;
+
+procedure CalculateSharpBounds(ALeft, ATop, ARight, ABottom,
+  LineWidth: Integer; out Bounds: TDoubleRect);
+var
+  Offset: Double;
+begin
+  Offset := GetSharpLineOffset(LineWidth);
+  with Bounds do
+  begin
+    Left := ALeft + Offset;
+    Top := ALeft + Offset;
+    Right := ARight - Offset;
+    Bottom := ABottom - Offset;
+  end;
+end;
+
+procedure CalculateSharpRect(X, Y, Width, Height, LineWidth: Integer; out
+  Rect: TDoubleRect);
+var
+  Offset: Double;
+begin
+  Offset := GetSharpLineOffset(LineWidth);
+  with Rect do
+  begin
+    Left := X + Offset;
+    Top := Y + Offset;
+    Right := Width - (2 * Offset);
+    Bottom := Height - (2 * Offset);
+  end;
 end;
 
 function RadianToDegree(const Angle: Double): Double;
