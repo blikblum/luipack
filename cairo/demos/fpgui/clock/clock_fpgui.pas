0fe6e6fb44be2fce041bec8be886fa2bb211e553
blikblum <blikblum@d0c4a878-0f54-0410-8585-a5e1b55c9de8>
2008-05-15 00:34:26 +0000

    * Cairo  - Implemented CairoFpGui surface under x11  - Implemented TfpgCustoCairoControl and TfpgCairoPaintBox  - Added demos to show usage of fpgui + cairo

---
+++ cairo/demos/fpgui/clock/clock_fpgui.pas
@@ -0,0 +1,188 @@
+program clock_fpgui;
+
+{$mode objfpc}{$H+}
+
+uses
+  {$IFDEF UNIX}{$IFDEF UseCThreads}
+  cthreads,
+  {$ENDIF}{$ENDIF}
+  Classes, SysUtils,
+  gfxbase, fpgfx, gui_form, gfx_imgfmt_bmp, CairofpGui, CairoClasses, CairoUtils, cairo14;
+
+const
+  m_radius = 0.42;
+  m_lineWidth = 0.05;
+
+type
+
+  { TMainForm }
+
+  TMainForm = class(TfpgForm)
+  private
+  protected
+  public
+    Timer: TfpgTimer;
+    PaintBox: TfpgCairoPaintBox;
+    constructor Create(AOwner: TComponent); override;
+    procedure   TimerFired(Sender: TObject);
+    procedure   FormDestroy(Sender: TObject);
+    procedure   FormClose(Sender: TObject; var CloseAction: TCloseAction);
+    procedure   AfterCreate; override;
+    procedure PaintBoxDraw(Sender: TObject);
+  end;
+
+constructor TMainForm.Create(AOwner: TComponent);
+begin
+  inherited Create(AOwner);
+  PaintBox := TfpgCairoPaintBox.Create(Self);
+  with PaintBox do
+  begin
+    Parent := Self;
+    Left := 0;
+    Top := 0;
+    Width := Self.Width;
+    Height := Self.Height;
+    Visible := True;
+    Align := alClient;
+    OnDraw := @PaintBoxDraw;
+  end;
+  Timer := TfpgTimer.Create(1000);
+  Timer.OnTimer := @TimerFired;
+  Timer.Enabled := True;
+end;
+
+procedure TMainForm.FormClose(Sender: TObject; var CloseAction: TCloseAction);
+begin
+  Timer.Enabled := False;
+end;
+
+procedure TMainForm.FormDestroy(Sender: TObject);
+begin
+  Timer.Destroy;
+end;
+
+procedure TMainForm.PaintBoxDraw(Sender: TObject);
+var
+  i : Integer;
+  Hour, Second, Minute, MSecond: Word;
+  inset, HourAngle, MinuteAngle, SecondAngle: Double;
+begin
+  with PaintBox, Context do
+  begin
+    // scale to unit square and translate (0, 0) to be (0.5, 0.5), i.e.
+    // the center of the window
+    Scale(Width, Height);
+    Translate(0.5, 0.5);
+    LineWidth := m_lineWidth;
+
+    Save;
+    SetSourceRGBA(0.337, 0.612, 0.117, 0.9);   // green
+    Paint;
+    Restore;
+
+    Arc(0, 0, m_radius, 0, 2 * PI);
+
+    Save;
+    SetSourceRGBA(1.0, 1.0, 1.0, 0.8);
+    FillPreserve;
+    Restore;
+    
+    StrokePreserve;
+    Clip;
+
+    //clock ticks
+    for i := 0 to 11 do
+    begin
+      inset := 0.05;
+      Save;
+      LineCap := CAIRO_LINE_CAP_ROUND;
+
+      if (i div 3 <> 0) then
+      begin
+        inset := inset*0.8;
+        LineWidth := 0.03;
+      end;
+
+      MoveTo( (m_radius - inset) * cos (i * PI / 6),
+              (m_radius - inset) * sin (i * PI / 6));
+      LineTo (m_radius * cos (i * PI / 6),
+              m_radius * sin (i * PI / 6));
+      Stroke;
+      Restore; (* stack-pen-size *)
+    end;
+
+    // store the current time
+    DecodeTime(Time, Hour, Minute, Second, MSecond);
+
+    // compute the angles of the indicators of our clock
+    MinuteAngle := Minute * PI / 30;
+    HourAngle := hour * PI / 6;
+    SecondAngle := Second * PI / 30;
+
+    Save;
+    LineCap := CAIRO_LINE_CAP_ROUND;
+
+    // draw the seconds hand
+    Save;
+    LineWidth := m_lineWidth / 3;
+    SetSourceRGBA(0.7, 0.7, 0.7, 0.8); // gray
+    MoveTo(0, 0);
+    LineTo(sin(SecondAngle) * (m_radius * 0.9),
+            -cos(SecondAngle) * (m_radius * 0.9));
+    Stroke;
+    Restore;
+
+    // draw the minutes hand
+    SetSourceRGBA(0.117, 0.337, 0.612, 0.9);   // blue
+    MoveTo(0, 0);
+    LineTo(sin(MinuteAngle + SecondAngle / 60) * (m_radius * 0.8),
+            -cos(MinuteAngle + SecondAngle / 60) * (m_radius * 0.8));
+    Stroke;
+
+    // draw the hours hand
+    SetSourceRGBA(0.337, 0.612, 0.117, 0.9);   // green
+    MoveTo(0, 0);
+    LineTo(sin(HourAngle + MinuteAngle / 12.0) * (m_radius * 0.5),
+            -cos(HourAngle + MinuteAngle / 12.0) * (m_radius * 0.5));
+    Stroke;
+    Restore;
+
+    // draw a little dot in the middle
+    Arc(0, 0, m_lineWidth / 3.0, 0, 2 * PI);
+    Fill;
+  end;
+end;
+
+procedure TMainForm.TimerFired(Sender: TObject);
+begin
+  PaintBox.Invalidate;
+end;
+
+procedure TMainForm.AfterCreate;
+begin
+  Name := 'MainForm';
+  SetPosition(316, 186, 400, 400);
+  WindowTitle := 'Cairo Clock Demo Running Under FpGui';
+  WindowPosition := wpScreenCenter;
+end;
+
+
+procedure MainProc;
+var
+  frm: TMainForm;
+begin
+  fpgApplication.Initialize;
+  frm := TMainForm.Create(nil);
+  try
+    frm.Show;
+    fpgApplication.Run;
+  finally
+    frm.Free;
+  end;
+end;
+
+begin
+  MainProc;
+end.
+
+
--- cairo/fpgui/cairofpgui.pas
+++ cairo/fpgui/cairofpgui.pas
@@ -1,30 +1,159 @@
 unit CairofpGui;
 
+{
+  Integrates Cairo with fpGui
+
+  Copyright (C) 2008 Luiz Americo Pereira Camara
+  pascalive@bol.com.br
+
+  This library is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Library General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or (at your
+  option) any later version with the following modification:
+
+  As a special exception, the copyright holders of this library give you
+  permission to link this library with independent modules to produce an
+  executable, regardless of the license terms of these independent modules,and
+  to copy and distribute the resulting executable under terms of your choice,
+  provided that you also meet, for each linked independent module, the terms
+  and conditions of the license of that module. An independent module is a
+  module which is not derived from or based on this library. If you modify
+  this library, you may extend this exception to your version of the library,
+  but you are not obligated to do so. If you do not wish to do so, delete this
+  exception statement from your version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Library General Public License
+  for more details.
+
+  You should have received a copy of the GNU Library General Public License
+  along with this library; if not, write to the Free Software Foundation,
+  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+}
+
+
 {$mode objfpc}{$H+}
 
 interface
 
 uses
-  Classes, SysUtils, fpgfx, CairoClasses;
+  Classes, SysUtils, CairoClasses, Cairo14, fpgfx, gfx_widget;
 
 type
 
-  { TCairofpgSurface }
+  { TfpgCairoSurface }
 
-  TCairofpgSurface = class(TCairoSurface)
+  TfpgCairoSurface = class(TCairoSurface)
   public
     constructor Create(Canvas: TfpgCanvas);
   end;
   
+  { TfpgCustomCairoControl }
+
+  TfpgCustomCairoControl = class(TfpgWidget)
+  private
+    FContext: TCairoContext;
+    FOnCreateContext: TNotifyEvent;
+  protected
+    procedure DoCreateContext; virtual;
+    procedure DoDraw; virtual; abstract;
+    procedure HandlePaint; override;
+    property Context: TCairoContext read FContext;
+    property OnCreateContext: TNotifyEvent read FOnCreateContext write FOnCreateContext;
+  public
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy; override;
+  end;
+  
+  { TfpgCairoPaintBox }
+
+  TfpgCairoPaintBox = class(TfpgCustomCairoControl)
+  private
+    FOnDraw: TNotifyEvent;
+  protected
+    procedure DoDraw; override;
+  public
+    property Context;
+  published
+    constructor Create(AOwner: TComponent); override;
+    property OnDraw: TNotifyEvent read FOnDraw write FOnDraw;
+  end;
+
+
   function CreateSurfaceFromCanvas(Canvas: TfpgCanvas): Pcairo_surface_t;
   
 implementation
 
-{ TCairofpgSurface }
 
-constructor TCairofpgSurface.Create(Canvas: TfpgCanvas);
+{$ifdef windows}
+{$i cairo_gdi.inc}
+{$endif}
+
+{$ifdef unix}
+{$i cairo_x11.inc}
+{$endif}
+
+
+{ TfpgCairoSurface }
+
+constructor TfpgCairoSurface.Create(Canvas: TfpgCanvas);
 begin
   FHandle := CreateSurfaceFromCanvas(Canvas);
+end;
+
+
+{ TfpgCustomCairoControl }
+
+constructor TfpgCustomCairoControl.Create(AOwner: TComponent);
+begin
+  inherited Create(AOwner);
+end;
+
+destructor TfpgCustomCairoControl.Destroy;
+begin
+  inherited Destroy;
+end;
+
+procedure TfpgCustomCairoControl.DoCreateContext;
+begin
+  if Assigned(FOnCreateContext) then
+    FOnCreateContext(Self);
+end;
+
+procedure TfpgCustomCairoControl.HandlePaint;
+var
+  Surface: Pcairo_surface_t;
+begin
+  Surface := CreateSurfaceFromCanvas(Canvas);
+  FContext := TCairoContext.Create(Surface);
+  cairo_surface_destroy(Surface);
+  try
+    Canvas.Color := BackgroundColor;
+    Canvas.FillRectangle(0,0,Width,Height);
+    DoCreateContext;
+    DoDraw;
+  finally
+    FreeAndNil(FContext);
+  end;
+end;
+
+
+{ TfpgCairoPaintBox }
+
+constructor TfpgCairoPaintBox.Create(AOwner: TComponent);
+begin
+  inherited Create(AOwner);
+end;
+
+procedure TfpgCairoPaintBox.DoDraw;
+begin
+  if Assigned(FOnDraw) then
+  begin
+    FContext.Save;
+    OnDraw(Self);
+    FContext.Restore;
+  end;
 end;
 
 end.
