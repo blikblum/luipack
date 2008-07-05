cd0560cb4c88b67c68bb4016b3b5ad957758aca6
blikblum <blikblum@d0c4a878-0f54-0410-8585-a5e1b55c9de8>
2008-07-05 12:48:00 +0000

    * Cairo  - Updated the vampyre imaging packages

---
+++ cairo/imaging/extra/vampyreimagingpackage.pas
@@ -0,0 +1,20 @@
+{ This file was automatically created by Lazarus. Do not edit!
+This source is only used to compile and install the package.
+ }
+
+unit vampyreimagingpackage; 
+
+interface
+
+uses
+  ImagingComponents, LazarusPackageIntf; 
+
+implementation
+
+procedure Register; 
+begin
+end; 
+
+initialization
+  RegisterPackage('vampyreimagingpackage', @Register); 
+end.
