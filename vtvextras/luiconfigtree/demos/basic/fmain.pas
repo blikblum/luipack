09448b9d098dc8d80639506f0a2960b65bef4ceb
luizamericop <luizamericop@d0c4a878-0f54-0410-8585-a5e1b55c9de8>
2008-12-18 00:07:20 +0000

    * LuiConfigTree:  - Added demo

---
+++ vtvextras/luiconfigtree/demos/basic/fmain.pas
@@ -0,0 +1,85 @@
+unit fMain;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, LResources, Forms, Controls, Graphics, Dialogs,
+  LuiConfig, LuiConfigTree, IniConfigProvider, VirtualTrees, StdCtrls;
+
+type
+
+  { TMainForm }
+
+  TMainForm = class(TForm)
+    IniProvider: TIniConfigProvider;
+    ConfigTree: TLuiConfigTree;
+    Config: TLuiConfig;
+    IniFileMemo: TMemo;
+    procedure ConfigTreeBeforeCellPaint(Sender: TBaseVirtualTree;
+      TargetCanvas: TCanvas; Node: PVirtualNode; Column: TColumnIndex;
+      CellPaintMode: TVTCellPaintMode; CellRect: TRect; var ContentRect: TRect);
+    procedure ConfigTreeBeforeItemErase(Sender: TBaseVirtualTree;
+      TargetCanvas: TCanvas; Node: PVirtualNode; const ItemRect: TRect;
+      var ItemColor: TColor; var EraseAction: TItemEraseAction);
+    procedure ConfigTreeNewText(Sender: TBaseVirtualTree; Node: PVirtualNode;
+      Column: TColumnIndex; const NewText: UnicodeString);
+    procedure FormCreate(Sender: TObject);
+  private
+    procedure UpdateMemo;
+    { private declarations }
+  public
+    { public declarations }
+  end; 
+
+var
+  MainForm: TMainForm;
+
+implementation
+
+{ TMainForm }
+
+procedure TMainForm.ConfigTreeBeforeCellPaint(Sender: TBaseVirtualTree;
+  TargetCanvas: TCanvas; Node: PVirtualNode; Column: TColumnIndex;
+  CellPaintMode: TVTCellPaintMode; CellRect: TRect; var ContentRect: TRect);
+begin
+  if (Node^.Parent <> Sender.RootNode) and (Column = 0) then
+  begin
+    TargetCanvas.Brush.Color := $e0e0e0;
+    TargetCanvas.FillRect(CellRect);
+  end;
+end;
+
+procedure TMainForm.ConfigTreeBeforeItemErase(Sender: TBaseVirtualTree;
+  TargetCanvas: TCanvas; Node: PVirtualNode; const ItemRect: TRect;
+  var ItemColor: TColor; var EraseAction: TItemEraseAction);
+begin
+  if Node^.Parent = Sender.RootNode then
+  begin
+    EraseAction := eaColor;
+    ItemColor := $9f9f9f;
+  end;
+end;
+
+procedure TMainForm.ConfigTreeNewText(Sender: TBaseVirtualTree;
+  Node: PVirtualNode; Column: TColumnIndex; const NewText: UnicodeString);
+begin
+  UpdateMemo;
+end;
+
+procedure TMainForm.FormCreate(Sender: TObject);
+begin
+  UpdateMemo;
+end;
+
+procedure TMainForm.UpdateMemo;
+begin
+  IniFileMemo.Lines.LoadFromFile('test.ini');
+end;
+
+initialization
+  {$I fmain.lrs}
+
+end.
+
