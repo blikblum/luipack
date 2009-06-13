f2b1674263b85e6f82c12e82e6c1902f599b543e
luizamericop <luizamericop@d0c4a878-0f54-0410-8585-a5e1b55c9de8>
2009-06-13 23:34:52 +0000

    * SqliteExtensions  - Finish implementation of TSqliteQueryBuilder

--- sqlite/extensions/sqliteutils.pas
+++ sqlite/extensions/sqliteutils.pas
@@ -5,7 +5,7 @@ unit SqliteUtils;
 interface
 
 uses
-  Classes, SysUtils, CustomSqliteDS, fgl, strutils, db;
+  Classes, SysUtils, CustomSqliteDS, fgl, db;
 
 type
 
@@ -17,7 +17,7 @@ type
 
   { TSqliteQueryBuilder }
 
-  TSqliteQueryBuilder = class
+  TSqliteQueryBuilder = class(TComponent)
   private
     FDataset: TCustomSqliteDataset;
     FExcludeFields: TStrings;
@@ -32,114 +32,173 @@ type
     FWorkSql: TStrings;
     FWorkTableName: String;
     //templates
+    FDeleteTemplate: String;
     FInsertTemplate: String;
     FUpdateTemplate: String;
     FPrimaryKeyTemplate: String;
+    FFieldValues: array of String;
+    FFieldPrimaryKey: array of String;
     procedure BuildTemplates;
-    procedure SetDataset(const AValue: TCustomSqliteDataset);
-    procedure SetOptions(const AValue: TSqliteQueryBuilderOptions);
+    function FormatPrimaryKeyList(Values: PPChar): String;
+    function FormatValueList(const Template: String; Values: PPChar): String;
     procedure ParseUpdate(UserData: Pointer; Values: PPChar; ABookmark: TBookmark; State: TRecordState);
-    procedure SetRecordStates(const AValue: TRecordStateSet);
-    procedure SetTableName(const AValue: String);
+    procedure SetExcludeFields(const AValue: TStrings);
+    procedure SetIncludeFields(const AValue: TStrings);
   public
-    constructor Create;
+    constructor Create(AOwner: TComponent); override;
     destructor Destroy; override;
     procedure Execute;
-    property Dataset: TCustomSqliteDataset read FDataset write SetDataset;
-    property ExcludeFields: TStrings read FExcludeFields;
-    property IncludeFields: TStrings read FIncludeFields;
-    property Options: TSqliteQueryBuilderOptions read FOptions write SetOptions;
-    property PrimaryKey: TStrings read FPrimaryKey;
-    property RecordStates: TRecordStateSet read FRecordStates write SetRecordStates;
     property SQL: TStrings read FSQL;
-    property TableName: String read FTableName write SetTableName;
+  published
+    property Dataset: TCustomSqliteDataset read FDataset write FDataset;
+    property ExcludeFields: TStrings read FExcludeFields write SetExcludeFields;
+    property IncludeFields: TStrings read FIncludeFields write SetIncludeFields;
+    property Options: TSqliteQueryBuilderOptions read FOptions write FOptions default [sqoSaveToSqlList];
+    property PrimaryKey: TStrings read FPrimaryKey;
+    property RecordStates: TRecordStateSet read FRecordStates write FRecordStates default [rsAdded, rsUpdated, rsDeleted];
+    property TableName: String read FTableName write FTableName;
   end;
 
 implementation
+
+//replace parameters
+//modified from StrUtils StringsReplace
+function ReplaceParams(const S: string; Pattern: array of string): string;
+
+var pc,pcc,lastpc : pchar;
+    strcount      : integer;
+    ResStr,
+    CompStr,
+    ParamIndex    : string;
+    Found         : Boolean;
+    sc            : integer;
+
+begin
+  sc := length(Pattern);
+  dec(sc);
+
+  CompStr := s;
+
+  ResStr := '';
+  pc := @CompStr[1];
+  pcc := @s[1];
+  lastpc := pc+Length(S);
+
+  while pc < lastpc do
+    begin
+    Found := False;
+    for strcount := 0 to sc do
+      begin
+      ParamIndex := '%' + IntToStr(strcount) + '%';
+      if (ParamIndex[1]=pc^) and
+         (Length(ParamIndex) <= (lastpc-pc)) and
+         (CompareByte(ParamIndex[1],pc^,Length(ParamIndex))=0) then
+        begin
+        ResStr := ResStr + Pattern[strcount];
+        pc := pc+Length(ParamIndex);
+        pcc := pcc+Length(ParamIndex);
+        Found := true;
+        end
+      end;
+    if not found then
+      begin
+      ResStr := ResStr + pcc^;
+      inc(pc);
+      inc(pcc);
+      end;
+    end;
+  Result := ResStr;
+end;
 
 { TSqliteQueryBuilder }
 
 procedure TSqliteQueryBuilder.BuildTemplates;
 var
   i: Integer;
-  FieldName: String;
+  FieldName, ValueList: String;
 begin
+  FDeleteTemplate := 'DELETE FROM ' + FWorkTableName + ' WHERE ';
   FInsertTemplate := 'INSERT INTO ' + FWorkTableName + '(';
   FUpdateTemplate := 'UPDATE ' + FWorkTableName + ' SET ';
+  ValueList := '';
   for i := 0 to FFieldList.Count - 2 do
   begin
+    ValueList := ValueList + '%' + IntToStr(i) + '%,';
     FieldName := FDataset.FieldDefs[i].Name;
     FInsertTemplate := FInsertTemplate + FieldName + ',';
-    FUpdateTemplate := FUpdateTemplate + FieldName + ' = %' + IntToStr(i) + ',';
+    FUpdateTemplate := FUpdateTemplate + FieldName + ' = %' + IntToStr(i) + '%,';
   end;
   //set the last field
   i := FFieldList.Count - 1;
   FieldName := FDataset.FieldDefs[i].Name;
-  FInsertTemplate := FInsertTemplate + FieldName + ') VALUES (%v);';
-  FUpdateTemplate := FUpdateTemplate + FieldName + '= %' + IntToStr(i) + ' WHERE ';
+  ValueList := ValueList + '%' + IntToStr(i) + '%';
+  FInsertTemplate := FInsertTemplate + FieldName + ') VALUES (' + ValueList + ');';
+  FUpdateTemplate := FUpdateTemplate + FieldName + '= %' + IntToStr(i) + '% WHERE ';
 
   //primary key
   FPrimaryKeyTemplate := '';
   for i := 0 to FPrimaryKeyList.Count - 1 do
   begin
     FieldName := FDataset.FieldDefs[i].Name;
-    FPrimaryKeyTemplate := FPrimaryKeyTemplate + FieldName + ' = %' + IntToStr(i);
+    FPrimaryKeyTemplate := FPrimaryKeyTemplate + FieldName + ' = %' + IntToStr(i) + '%';
     if i < FPrimaryKeyList.Count - 1 then
       FPrimaryKeyTemplate := FPrimaryKeyTemplate + ' AND '
   end;
   FPrimaryKeyTemplate := FPrimaryKeyTemplate + ';';
 end;
 
-procedure TSqliteQueryBuilder.SetDataset(const AValue: TCustomSqliteDataset);
+function TSqliteQueryBuilder.FormatPrimaryKeyList(Values: PPChar): String;
+var
+  i: Integer;
 begin
-  if FDataset = AValue then exit;
-  FDataset := AValue;
+  for i := 0 to FPrimaryKeyList.Count - 1 do
+    FFieldPrimaryKey[i] := FDataset.GetSQLValue(Values, FPrimaryKeyList[i]);
+  Result := ReplaceParams(FPrimaryKeyTemplate, FFieldPrimaryKey);
 end;
 
-procedure TSqliteQueryBuilder.SetOptions(
-  const AValue: TSqliteQueryBuilderOptions);
+function TSqliteQueryBuilder.FormatValueList(const Template: String;
+  Values: PPChar): String;
+var
+  i: Integer;
 begin
-  if FOptions = AValue then exit;
-  FOptions := AValue;
+  for i := 0 to FFieldList.Count - 1 do
+    FFieldValues[i] := FDataset.GetSQLValue(Values, FFieldList[i]);
+  Result := ReplaceParams(Template, FFieldValues);
 end;
 
 procedure TSqliteQueryBuilder.ParseUpdate(UserData: Pointer; Values: PPChar;
   ABookmark: TBookmark; State: TRecordState);
-var
-  i: Integer;
-
 begin
   case State of
     rsAdded:
     begin
-
+      FWorkSql.Add(FormatValueList(FInsertTemplate, Values));
     end;
     rsDeleted:
     begin
-
+      FWorkSql.Add(FDeleteTemplate + FormatPrimaryKeyList(Values));
     end;
     rsUpdated:
     begin
-
-
+      FWorkSql.Add(FormatValueList(FUpdateTemplate, Values) +
+        FormatPrimaryKeyList(Values));
     end;
   end;
 end;
 
-procedure TSqliteQueryBuilder.SetRecordStates(const AValue: TRecordStateSet);
+procedure TSqliteQueryBuilder.SetExcludeFields(const AValue: TStrings);
 begin
-  if FRecordStates = AValue then exit;
-  FRecordStates := AValue;
+  FExcludeFields.Assign(AValue);
 end;
 
-procedure TSqliteQueryBuilder.SetTableName(const AValue: String);
+procedure TSqliteQueryBuilder.SetIncludeFields(const AValue: TStrings);
 begin
-  if FTableName = AValue then exit;
-  FTableName := AValue;
+  FIncludeFields.Assign(AValue);
 end;
 
-constructor TSqliteQueryBuilder.Create;
+constructor TSqliteQueryBuilder.Create(AOwner: TComponent);
 begin
+  inherited Create(AOwner);
   FIncludeFields := TStringList.Create;
   FExcludeFields := TStringList.Create;
   FSQL := TStringList.Create;
@@ -189,6 +248,7 @@ begin
     FFieldList.Remove(FDataset.FieldDefs.IndexOf(FExcludeFields[i]));
   if FFieldList.Count = 0 then
     raise Exception.Create('No field specified');
+  SetLength(FFieldValues, FFieldList.Count);
 
   //build primary key list
   EmptyPrimaryKey := FPrimaryKey.Count = 0;
@@ -205,6 +265,7 @@ begin
     Exception.Create('A primary key must be set for updated or deleted records');
   if EmptyPrimaryKey then
     FPrimaryKey.Clear;
+  SetLength(FFieldPrimaryKey, FPrimaryKeyList.Count);
 
   if sqoSaveToSqlList in FOptions then
     FWorkSql := FDataset.SQLList
