4e0ba2ddbe55a3df032262416a6d9163be9e7dd5
luizamericop <luizamericop@d0c4a878-0f54-0410-8585-a5e1b55c9de8>
2010-03-24 03:46:09 +0000

    * Multilog - Add Lazarus project for generic demo - Add test for sending TComponent in generic demo

--- multilog/demos/testmultilog.pas
+++ multilog/demos/testmultilog.pas
@@ -1,13 +1,28 @@
 program testmultilog;
+
 {$ifdef fpc}
 {$mode objfpc}{$H+}
 {$endif}
 
 uses
-  Classes, SysUtils
-  { add your units here },filechannel, ipcchannel, sharedlogger;
+  Classes, SysUtils,
+  filechannel, ipcchannel, sharedlogger;
+
+type
+
+  { TMyObject }
+
+  TMyObject = class(TComponent)
+  private
+    FId: Integer;
+  published
+    property Id: Integer read FId write FId;
+  end;
+
 var
   AList: TStrings;
+  Obj: TMyObject;
+
 begin
   AList:=TStringList.Create;
   with Logger do
@@ -16,26 +31,33 @@ begin
     Channels.Add(TIPCChannel.Create);
     ActiveClasses:=[0,1];
     DefaultClasses:= [1];
-	IncCounter('Counter1');
-	Watch('AStrWatch','XXXX');
+    IncCounter('Counter1');
+    Watch('AStrWatch','XXXX');
     Send('An empty StringList',AList);
     IncCounter('CounteR1');
-	Watch('AIntWatch',123);
-	Send('A Text Message');    	
-	IncCounter('CounTER1');	
-	Send('Another Text Message');   
+    Watch('AIntWatch',123);
+    Send('A Text Message');
+    IncCounter('CounTER1');
+    Send('Another Text Message');
     with AList do
     begin
       Add('aaaaaa');
       Add('bbbbbb');
       Add('cccccc'); 
     end;
-	DecCounter('Counter1');
-	Watch('AIntWatch',321);  
+    DecCounter('Counter1');
+    Watch('AIntWatch',321);
     SendError('A Error Message');
-	ResetCounter('Counter1');
+    ResetCounter('Counter1');
     Watch('ASTRWatch','YYYY');
-	EnterMethod('DoIt');
+
+    Obj := TMyObject.Create(nil);
+    Obj.Name := 'The_Obj_Name';
+    Obj.Id := 2;
+    Send('An Object/Component', Obj);
+    Obj.Destroy;
+
+    EnterMethod('DoIt');
     Send('AText inside DoIt');
     SendWarning('A Warning');
     Send('A String','sadjfgadsfbmsandfb');
