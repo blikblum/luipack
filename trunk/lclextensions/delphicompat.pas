8066b6393e61006dd8e35d311a35ea6a58cddebb
luizamericop <luizamericop@d0c4a878-0f54-0410-8585-a5e1b55c9de8>
2010-06-22 21:54:02 +0000

    * LCLExtensions  - Remove mapping modes constants

--- trunk/lclextensions/delphicompat.pas
+++ trunk/lclextensions/delphicompat.pas
@@ -83,16 +83,6 @@ const
   RDW_UPDATENOW = 256;
   RDW_ALLCHILDREN = 128;
   RDW_NOCHILDREN = 64;
-
-  { Mapping Modes }
-  MM_TEXT        = 1;
-  MM_LOMETRIC    = 2;
-  MM_HIMETRIC    = 3;
-  MM_LOENGLISH   = 4;
-  MM_HIENGLISH   = 5;
-  MM_TWIPS       = 6;
-  MM_ISOTROPIC   = 7;
-  MM_ANISOTROPIC = 8;
   
   { DRAWITEMSTRUCT structure  }
   ODT_BUTTON = 4;
