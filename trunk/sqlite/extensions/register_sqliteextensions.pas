b5a0d5ff1877ec8360d1c6fac518b060b2c2c510
luizamericop <luizamericop@d0c4a878-0f54-0410-8585-a5e1b55c9de8>
2010-08-24 19:10:40 +0000

    * SqliteExtensions  - Register TSqlite3Database in the IDE  - Publish TSqlite3Database.FileName

--- trunk/sqlite/extensions/register_sqliteextensions.pas
+++ trunk/sqlite/extensions/register_sqliteextensions.pas
@@ -5,7 +5,7 @@ unit register_sqliteextensions;
 interface
 
 uses
-  Classes, SysUtils, LazarusPackageIntf, SqliteUtils;
+  Classes, SysUtils, LazarusPackageIntf, SqliteUtils, Sqlite3Wrapper;
 
 procedure Register;
 
@@ -16,9 +16,16 @@ begin
   RegisterComponents('Data Access', [TSqliteQueryBuilder]);
 end;
 
+procedure RegisterUnitSqlite3Wrapper;
+begin
+  RegisterComponents('Data Access', [TSqlite3Database]);
+end;
+
+
 procedure Register;
 begin
   RegisterUnit('SqliteUtils', @RegisterUnitSqliteUtils);
+  RegisterUnit('Sqlite3Wrapper', @RegisterUnitSqlite3Wrapper);
 end;
 
 end.
