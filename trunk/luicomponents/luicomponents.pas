44432ccc4891a4573f01f68a49d01e021a3174dc
luizamericop <luizamericop@d0c4a878-0f54-0410-8585-a5e1b55c9de8>
2011-11-06 00:01:40 +0000

    * LuiComponents - (Very) initial implementation of ObjectPool

--- trunk/luicomponents/luicomponents.pas
+++ trunk/luicomponents/luicomponents.pas
@@ -9,7 +9,7 @@ interface
 uses
   LuiConfig, IniConfigProvider, register_luicomponents, LuiOrderedDataset, 
   LuiRecordBuffer, LuiDateUtils, LuiRTTIUtils, LuiStrUtils, LuiMiscUtils, 
-  LuiJSONUtils, LuiDBExport, LazarusPackageIntf;
+  LuiJSONUtils, LuiDBExport, LuiObjectUtils, LuiServices, LazarusPackageIntf;
 
 implementation
 
