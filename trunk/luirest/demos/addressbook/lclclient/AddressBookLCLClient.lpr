59e8ae90aa6780a3966c61463dde3612a8562cfd
luizamericop <luizamericop@d0c4a878-0f54-0410-8585-a5e1b55c9de8>
2012-11-17 16:57:24 +0000

    LuiRest + Add Addessbook lclclient demo

---
+++ trunk/luirest/demos/addressbook/lclclient/AddressBookLCLClient.lpr
@@ -0,0 +1,21 @@
+program AddressBookLCLClient;
+
+{$mode objfpc}{$H+}
+
+uses
+  {$IFDEF UNIX}{$IFDEF UseCThreads}
+  cthreads,
+  {$ENDIF}{$ENDIF}
+  Interfaces, // this includes the LCL widgetset
+  Forms, luicomponents, laz_synapse, MainView, ContactView, PhoneView
+  { you can add units after this };
+
+{$R *.res}
+
+begin
+  RequireDerivedFormResource := True;
+  Application.Initialize;
+  Application.CreateForm(TMainForm, MainForm);
+  Application.Run;
+end.
+
---
+++ trunk/luirest/demos/addressbook/lclclient/contactview.pas
@@ -0,0 +1,63 @@
+unit ContactView;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
+  Buttons, fpjson;
+
+type
+
+  { TContactViewForm }
+
+  TContactViewForm = class(TForm)
+    BitBtn1: TBitBtn;
+    BitBtn2: TBitBtn;
+    NameEdit: TLabeledEdit;
+    procedure BitBtn1Click(Sender: TObject);
+  private
+    FData: TJSONObject;
+    procedure SetData(AValue: TJSONObject);
+    { private declarations }
+  public
+    { public declarations }
+    property Data: TJSONObject read FData write SetData;
+    class function EditData(AOwner: TCustomForm; ContactData: TJSONObject): Boolean;
+  end;
+
+var
+  ContactViewForm: TContactViewForm;
+
+implementation
+
+{$R *.lfm}
+
+{ TContactViewForm }
+
+procedure TContactViewForm.BitBtn1Click(Sender: TObject);
+begin
+  FData.Strings['name'] := NameEdit.Text;
+end;
+
+procedure TContactViewForm.SetData(AValue: TJSONObject);
+begin
+  FData := AValue;
+  NameEdit.Text := AValue.Strings['name'];
+end;
+
+class function TContactViewForm.EditData(AOwner: TCustomForm;
+  ContactData: TJSONObject): Boolean;
+begin
+  with TContactViewForm.Create(AOwner) do
+   try
+     Data := ContactData;
+     Result := ShowModal = mrOK;
+   finally
+     Destroy;
+   end;
+end;
+
+end.
+
