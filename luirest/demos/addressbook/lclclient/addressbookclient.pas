33611a1cf0602cf3e272d9f8a846398f3269c739
luizamericop <luizamericop@d0c4a878-0f54-0410-8585-a5e1b55c9de8>
2012-12-15 00:38:45 +0000

    LuiREST * Simplify client /adapt demo

--- luirest/demos/addressbook/lclclient/addressbookclient.pas
+++ luirest/demos/addressbook/lclclient/addressbookclient.pas
@@ -16,7 +16,9 @@ const
 type
   THTTPMethodType = (hmtGet, hmtPost, hmtPut, hmtDelete);
 
-  TRESTResponseEvent = procedure(ResourceId: Integer; Method: THTTPMethodType; ResponseCode: Integer; ResponseStream: TStream) of object;
+  TRESTResponseEvent = procedure(ResourceTag: PtrInt; Method: THTTPMethodType; ResponseCode: Integer; ResponseStream: TStream) of object;
+
+  TSocketError = procedure(Sender: TObject; ErrorCode: Integer; const ErrorDescription: String) of object;
 
   { TAddressBookRESTClient }
 
@@ -25,53 +27,41 @@ type
   private
     FBaseURL: String;
     FHttpClient: THTTPSend;
-    FOnError: TRESTResponseEvent;
-    FOnSuccess: TRESTResponseEvent;
-    FQueryParams: String;
-    FResourceDefs: TJSONObject;
-    procedure DoResponseCallback(ResourceId: Integer; Method: THTTPMethodType; ResponseCode: Integer; ResponseStream: TStream);
-    function GetPath(ResourceId: PtrInt; const Args: array of const): String;
+    FOnResponseError: TRESTResponseEvent;
+    FOnResponseSuccess: TRESTResponseEvent;
+    FOnSocketError: TSocketError;
+    procedure DoResponseCallback(ResourcePath: PtrInt; Method: THTTPMethodType; ResponseCode: Integer; ResponseStream: TStream);
     procedure SetBaseURL(const AValue: String);
   public
     constructor Create(AOwner: TComponent); override;
     destructor Destroy; override;
-    function Delete(ResourceId: PtrInt; const Args: array of const): Boolean;
-    function Get(ResourceId: PtrInt; const Args: array of const): Boolean;
-    function Post(ResourceId: PtrInt; const Args: array of const; const DataStr: String): Boolean;
-    function Put(ResourceId: PtrInt; const Args: array of const; const DataStr: String): Boolean;
+    function Delete(const ResourcePath: String; ResourceTag: PtrInt): Boolean;
+    function Get(const ResourcePath: String; ResourceTag: PtrInt): Boolean;
+    function Post(const ResourcePath: String; ResourceTag: PtrInt; const Data: String): Boolean;
+    function Put(const ResourcePath: String; ResourceTag: PtrInt; const Data: String): Boolean;
     property BaseURL: String read FBaseURL write SetBaseURL;
-    property QueryParams: String read FQueryParams write FQueryParams;
-    property OnSuccess: TRESTResponseEvent read FOnSuccess write FOnSuccess;
-    property OnError: TRESTResponseEvent read FOnError write FOnError;
+    property OnResponseSuccess: TRESTResponseEvent read FOnResponseSuccess write FOnResponseSuccess;
+    property OnResponseError: TRESTResponseEvent read FOnResponseError write FOnResponseError;
+    property OnSocketError: TSocketError read FOnSocketError write FOnSocketError;
   end;
 
 implementation
 
 { TAddressBookRESTClient }
 
-procedure TAddressBookRESTClient.DoResponseCallback(ResourceId: Integer; Method: THTTPMethodType;
+procedure TAddressBookRESTClient.DoResponseCallback(ResourcePath: PtrInt; Method: THTTPMethodType;
    ResponseCode: Integer; ResponseStream: TStream);
 begin
   if ResponseCode < 300 then
   begin
-    if Assigned(FOnSuccess) then
-      FOnSuccess(ResourceId, Method, ResponseCode, ResponseStream);
+    if Assigned(FOnResponseSuccess) then
+      FOnResponseSuccess(ResourcePath, Method, ResponseCode, ResponseStream);
   end
   else
   begin
-    if Assigned(FOnError) then
-      FOnError(ResourceId, Method, ResponseCode, ResponseStream);
+    if Assigned(FOnResponseError) then
+      FOnResponseError(ResourcePath, Method, ResponseCode, ResponseStream);
   end;
-end;
-
-function TAddressBookRESTClient.GetPath(ResourceId: PtrInt; const Args: array of const): String;
-var
-  ResourceIndex: Integer;
-begin
-  ResourceIndex := FResourceDefs.IndexOfName(IntToStr(ResourceId));
-  if ResourceIndex = -1 then
-    raise Exception.CreateFmt('Unable to find resource "%d"', [ResourceId]);
-  Result := Format(FResourceDefs.Items[ResourceIndex].AsString, Args);
 end;
 
 procedure TAddressBookRESTClient.SetBaseURL(const AValue: String);
@@ -86,55 +76,80 @@ constructor TAddressBookRESTClient.Create(AOwner: TComponent);
 begin
   inherited Create(AOwner);
   FHttpClient := THTTPSend.Create;
-  FResourceDefs := TJSONObject.Create([
-    IntToStr(RES_CONTACTS), 'contacts',
-    IntToStr(RES_CONTACT), 'contacts/%s',
-    IntToStr(RES_CONTACTPHONES), 'contacts/%s/phones',
-    IntToStr(RES_CONTACTPHONE), 'contacts/%s/phones/%s'
-    ]);
 end;
 
 destructor TAddressBookRESTClient.Destroy;
 begin
   FHttpClient.Destroy;
-  FResourceDefs.Destroy;
   inherited Destroy;
 end;
 
-function TAddressBookRESTClient.Delete(ResourceId: PtrInt; const Args: array of const): Boolean;
+function TAddressBookRESTClient.Delete(const ResourcePath: String; ResourceTag: PtrInt): Boolean;
 begin
   FHttpClient.Clear;
-  Result := FHttpClient.HTTPMethod('DELETE', BaseURL + GetPath(ResourceId, Args));
+  Result := FHttpClient.HTTPMethod('DELETE', BaseURL + ResourcePath);
   if Result then
-    DoResponseCallback(ResourceId, hmtDelete, FHttpClient.ResultCode, FHttpClient.Document);
+  begin
+    Result := FHttpClient.ResultCode < 300;
+    DoResponseCallback(ResourceTag, hmtDelete, FHttpClient.ResultCode, FHttpClient.Document);
+  end
+  else
+  begin
+    if Assigned(FOnSocketError) then
+      FOnSocketError(Self, FHttpClient.Sock.LastError, FHttpClient.Sock.LastErrorDesc);
+  end;
 end;
 
-function TAddressBookRESTClient.Get(ResourceId: PtrInt; const Args: array of const): Boolean;
+function TAddressBookRESTClient.Get(const ResourcePath: String; ResourceTag: PtrInt): Boolean;
 begin
   FHttpClient.Clear;
-  Result := FHttpClient.HTTPMethod('GET', BaseURL + GetPath(ResourceId, Args));
+  Result := FHttpClient.HTTPMethod('GET', BaseURL + ResourcePath);
   if Result then
-    DoResponseCallback(ResourceId, hmtGet, FHttpClient.ResultCode, FHttpClient.Document);
+  begin
+    Result := FHttpClient.ResultCode < 300;
+    DoResponseCallback(ResourceTag, hmtGet, FHttpClient.ResultCode, FHttpClient.Document);
+  end
+  else
+  begin
+    if Assigned(FOnSocketError) then
+      FOnSocketError(Self, FHttpClient.Sock.LastError, FHttpClient.Sock.LastErrorDesc);
+  end;
 end;
 
-function TAddressBookRESTClient.Post(ResourceId: PtrInt; const Args: array of const;
-  const DataStr: String): Boolean;
+function TAddressBookRESTClient.Post(const ResourcePath: String; ResourceTag: PtrInt;
+  const Data: String): Boolean;
 begin
   FHttpClient.Clear;
-  FHttpClient.Document.Write(DataStr[1], Length(DataStr));
-  Result := FHttpClient.HTTPMethod('POST', BaseURL + GetPath(ResourceId, Args));
+  FHttpClient.Document.Write(Data[1], Length(Data));
+  Result := FHttpClient.HTTPMethod('POST', BaseURL + ResourcePath);
   if Result then
-    DoResponseCallback(ResourceId, hmtPost, FHttpClient.ResultCode, FHttpClient.Document);
+  begin
+    Result := FHttpClient.ResultCode < 300;
+    DoResponseCallback(ResourceTag, hmtPost, FHttpClient.ResultCode, FHttpClient.Document);
+  end
+  else
+  begin
+    if Assigned(FOnSocketError) then
+      FOnSocketError(Self, FHttpClient.Sock.LastError, FHttpClient.Sock.LastErrorDesc);
+  end;
 end;
 
-function TAddressBookRESTClient.Put(ResourceId: PtrInt; const Args: array of const;
-  const DataStr: String): Boolean;
+function TAddressBookRESTClient.Put(const ResourcePath: String; ResourceTag: PtrInt;
+  const Data: String): Boolean;
 begin
   FHttpClient.Clear;
-  FHttpClient.Document.Write(DataStr[1], Length(DataStr));
-  Result := FHttpClient.HTTPMethod('PUT', BaseURL + GetPath(ResourceId, Args));
+  FHttpClient.Document.Write(Data[1], Length(Data));
+  Result := FHttpClient.HTTPMethod('PUT', BaseURL + ResourcePath);
   if Result then
-    DoResponseCallback(ResourceId, hmtPut, FHttpClient.ResultCode, FHttpClient.Document);
+  begin
+    Result := FHttpClient.ResultCode < 300;
+    DoResponseCallback(ResourceTag, hmtPut, FHttpClient.ResultCode, FHttpClient.Document);
+  end
+  else
+  begin
+    if Assigned(FOnSocketError) then
+      FOnSocketError(Self, FHttpClient.Sock.LastError, FHttpClient.Sock.LastErrorDesc);
+  end;
 end;
 
 end.
