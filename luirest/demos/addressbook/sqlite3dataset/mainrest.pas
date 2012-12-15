35e3b55070434cb1f1102e022afdb929bfa60384
luizamericop <luizamericop@d0c4a878-0f54-0410-8585-a5e1b55c9de8>
2012-12-15 16:19:47 +0000

    LuiREST + Demo: Add option to return response in XML format

--- luirest/demos/addressbook/sqlite3dataset/mainrest.pas
+++ luirest/demos/addressbook/sqlite3dataset/mainrest.pas
@@ -14,6 +14,8 @@ type
   TMainRESTModule = class(TRESTServiceModule)
     AddressBookDataset: TSqlite3Dataset;
     procedure DataModuleCreate(Sender: TObject);
+    procedure DataModuleRequest(Sender: TObject; ARequest: TRequest;
+      AResponse: TResponse);
     procedure DataModuleResourceLoad(Resource: TRESTResource; ResourceTag: PtrInt);
   private
     { private declarations }
@@ -37,6 +39,23 @@ procedure TMainRESTModule.DataModuleCreate(Sender: TObject);
 begin
   AddressBookDataset.ExecSQL('PRAGMA foreign_keys = ON');
   Resources.Register('contacts', TContacts, 0);
+end;
+
+procedure TMainRESTModule.DataModuleRequest(Sender: TObject;
+  ARequest: TRequest; AResponse: TResponse);
+begin
+  if ARequest.QueryFields.Values['format'] = 'xml' then
+  begin
+    ResponseFormatter := TXMLRESTResponseFormatter;
+    ContentType := 'application/xml; charset=UTF-8';
+    TSqlite3DatasetResource.DatasetFormatter := TXMLDatasetResourceFormatter;
+  end
+  else
+  begin
+    ResponseFormatter := TJSONRESTResponseFormatter;
+    ContentType := 'application/json; charset=UTF-8';
+    TSqlite3DatasetResource.DatasetFormatter := TJSONDatasetResourceFormatter;
+  end;
 end;
 
 procedure TMainRESTModule.DataModuleResourceLoad(Resource: TRESTResource; ResourceTag: PtrInt);
