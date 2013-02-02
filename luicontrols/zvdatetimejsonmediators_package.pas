21882f8aee19602158e71278bc1a0a09417ac67c
luizamericop <luizamericop@gmail.com>
2013-02-02 23:42:23 +0000

    LuiControls + Add zvdatetime mediator

---
+++ luicontrols/zvdatetimejsonmediators_package.pas
@@ -0,0 +1,20 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit ZVDateTimeJSONMediators_package;
+
+interface
+
+uses
+  ZVDateTimeJSONMediators, LazarusPackageIntf;
+
+implementation
+
+procedure Register;
+begin
+end;
+
+initialization
+  RegisterPackage('ZVDateTimeJSONMediators_package', @Register);
+end.
