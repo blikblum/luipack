7c875d5ebbd5501bf210f1473371adda0a0b521f
luizamericop <luizamericop@gmail.com>
2013-02-03 01:20:59 +0000

    LuiComponents / DataClasses * Implement TJSONResourceLookup

--- luicomponents/luidataclasses.pas
+++ luicomponents/luidataclasses.pas
@@ -45,14 +45,120 @@ type
     FKeyProperty: String;
     FResource: IJSONArrayResource;
     FValueProperty: String;
+    function FindValueData(const KeyValue: Variant): TJSONData;
+    function GetKeys(Index: Integer): Variant;
+    function GetStrings(const KeyValue: Variant): String;
+    function GetValues(const KeyValue: Variant): Variant;
   public
+    constructor Create(AOwner: TComponent); override;
+    procedure AssignTo(Dest: TPersistent); override;
+    function IndexOf(const KeyValue: Variant): Integer;
+    procedure Update;
     property KeyProperty: String read FKeyProperty write FKeyProperty;
+    property Keys[Index: Integer]: Variant read GetKeys;
     property Resource: IJSONArrayResource read FResource write FResource;
     property ValueProperty: String read FValueProperty write FValueProperty;
+    property Values[KeyValue: Variant]: Variant read GetValues;
+    property Strings[KeyValue: Variant]: String read GetStrings;
   end;
 
 
 implementation
+
+uses
+  LuiJSONUtils;
+
+{ TJSONResourceLookup }
+
+function TJSONResourceLookup.GetValues(const KeyValue: Variant): Variant;
+var
+  ValueData: TJSONData;
+begin
+  ValueData := FindValueData(KeyValue);
+  if (ValueData <> nil) and not (ValueData.JSONType in [jtNull, jtArray, jtObject]) then
+    Result := ValueData.Value
+  else
+    Result := Null;
+end;
+
+constructor TJSONResourceLookup.Create(AOwner: TComponent);
+begin
+  inherited Create(AOwner);
+  FKeyProperty := 'id';
+  FValueProperty := 'name';
+end;
+
+procedure TJSONResourceLookup.AssignTo(Dest: TPersistent);
+var
+  i: Integer;
+  ItemData, ValueData: TJSONData;
+  ResourceData: TJSONArray;
+begin
+  if Dest is TStrings then
+  begin
+    ResourceData := FResource.Data;
+    TStrings(Dest).Clear;
+    for i := 0 to ResourceData.Count - 1 do
+    begin
+      ItemData := ResourceData.Items[i];
+      if ItemData.JSONType = jtObject then
+      begin
+        ValueData := TJSONObject(ItemData).Find(FValueProperty);
+        if (ValueData <> nil) and not (ValueData.JSONType in [jtNull, jtArray, jtObject]) then
+          TStrings(Dest).Add(ValueData.AsString);
+      end;
+    end;
+  end
+  else
+    inherited AssignTo(Dest);
+end;
+
+function TJSONResourceLookup.IndexOf(const KeyValue: Variant): Integer;
+begin
+  Result := GetJSONIndexOf(FResource.Data, [FKeyProperty, KeyValue]);
+end;
+
+procedure TJSONResourceLookup.Update;
+begin
+  FResource.Fetch;
+end;
+
+function TJSONResourceLookup.FindValueData(const KeyValue: Variant): TJSONData;
+var
+  ItemData: TJSONObject;
+begin
+  ItemData := FindJSONObject(FResource.Data, [FKeyProperty, KeyValue]);
+  if ItemData <> nil then
+    Result := ItemData.Find(FValueProperty)
+  else
+    Result := nil;
+end;
+
+function TJSONResourceLookup.GetKeys(Index: Integer): Variant;
+var
+  ResourceData: TJSONArray;
+  ItemData: TJSONData;
+begin
+  Result := Null;
+  ResourceData := FResource.Data;
+  if (Index >= 0) and (Index < ResourceData.Count) then
+  begin
+    ItemData := ResourceData.Items[Index];
+    if ItemData.JSONType = jtObject then
+      Result := TJSONObject(ItemData).Get(FKeyProperty);
+  end;
+end;
+
+function TJSONResourceLookup.GetStrings(const KeyValue: Variant): String;
+var
+  ValueData: TJSONData;
+begin
+  ValueData := FindValueData(KeyValue);
+  if (ValueData <> nil) and not (ValueData.JSONType in [jtNull, jtArray, jtObject]) then
+    Result := ValueData.AsString
+  else
+    Result := '';
+end;
 
 end.
 
