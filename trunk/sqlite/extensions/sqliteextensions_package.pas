e91f09f88e2119719233e656c4fa724f20e09831
luizamericop@gmail.com <luizamericop@gmail.com@d0c4a878-0f54-0410-8585-a5e1b55c9de8>
2013-03-02 10:51:59 +0000

    Sqlite Extensions + Initial import of a sqldb sqlite connection fork from trunk

--- trunk/sqlite/extensions/sqliteextensions_package.pas
+++ trunk/sqlite/extensions/sqliteextensions_package.pas
@@ -1,23 +1,22 @@
-{ This file was automatically created by Lazarus. do not edit ! 
+{ This file was automatically created by Lazarus. Do not edit!
   This source is only used to compile and install the package.
  }
 
-unit sqliteextensions_package; 
+unit sqliteextensions_package;
 
 interface
 
 uses
-    SqliteTrees, Sqlite3Wrapper, Sqlite3Ext, SqliteUtils, 
-  register_sqliteextensions, LazarusPackageIntf;
+  SqliteTrees, Sqlite3Wrapper, Sqlite3Ext, SqliteUtils, register_sqliteextensions, 
+  sqlite3slimconn, LazarusPackageIntf;
 
 implementation
 
-procedure Register; 
+procedure Register;
 begin
-  RegisterUnit('register_sqliteextensions', @register_sqliteextensions.Register
-    ); 
-end; 
+  RegisterUnit('register_sqliteextensions', @register_sqliteextensions.Register);
+end;
 
 initialization
-  RegisterPackage('sqliteextensions_package', @Register); 
+  RegisterPackage('sqliteextensions_package', @Register);
 end.
