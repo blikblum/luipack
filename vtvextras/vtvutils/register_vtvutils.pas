ee8b09ecec42e5cc11c587cb0a9f1d4c4a6a1f9c
luizamericop <luizamericop@gmail.com>
2013-08-28 02:35:09 +0000

    VTVUtils + Register QuestionTree in IDE

--- vtvextras/vtvutils/register_vtvutils.pas
+++ vtvextras/vtvutils/register_vtvutils.pas
@@ -5,7 +5,7 @@ unit register_vtvutils;
 interface
 
 uses
-  Classes, LazarusPackageIntf, VTJSON;
+  Classes, LazarusPackageIntf, VTJSON, VTJSONEx;
 
 procedure Register;
 
@@ -16,9 +16,15 @@ begin
   RegisterComponents('Virtual Controls', [TVirtualJSONInspector, TVirtualJSONListView, TVirtualJSONTreeView]);
 end;
 
+procedure RegisterUnitVTJSONEx;
+begin
+  RegisterComponents('Virtual Controls', [TJSONQuestionTreeView]);
+end;
+
 procedure Register;
 begin
   RegisterUnit('VTJSON', @RegisterUnitVTJSON);
+  RegisterUnit('VTJSONEx', @RegisterUnitVTJSONEx);
 end;
 
 end.
