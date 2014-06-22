bdbf97199e8cb0bf154762c130be9c5828a4024f
luizamericop <luizamericop@d0c4a878-0f54-0410-8585-a5e1b55c9de8>
2014-06-22 20:36:25 +0000

    LuiREST / CAM-ICU + Add evaluation view * Several fixes

--- luirest/demos/cam-icu/webclient/app/scripts/templates.js
+++ luirest/demos/cam-icu/webclient/app/scripts/templates.js
@@ -2,22 +2,26 @@ define(['handlebars'], function(Handlebars) {
 
 this["JST"] = this["JST"] || {};
 
-this["JST"]["app/scripts/templates/evaluation.hbs"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
-  this.compilerInfo = [4,'>= 1.0.0'];
-helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
-  
-
-
-  return "<p>Your content here.</p>\n\n";
-  });
-
 this["JST"]["app/scripts/templates/evaluationlistitem.hbs"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
-  
+  var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression;
 
 
-  return "<p>Your content here.</p>\n\n";
+  buffer += "<td>";
+  if (stack1 = helpers.date) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.date; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "</td>\r\n<td></td>\r\n<td>\r\n    <a href=\"#patients/";
+  if (stack1 = helpers.patientid) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.patientid; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "/evaluations/";
+  if (stack1 = helpers.id) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.id; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "\"><span class=\"glyphicon glyphicon-edit\"></span></a>\r\n</td>\r\n\r\n";
+  return buffer;
   });
 
 this["JST"]["app/scripts/templates/patient.hbs"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
