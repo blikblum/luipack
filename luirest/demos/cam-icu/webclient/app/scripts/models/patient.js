add1c8e1a5b5db782a02fa725677bddbd4a15524
luizamericop <luizamericop@d0c4a878-0f54-0410-8585-a5e1b55c9de8>
2014-06-30 02:33:19 +0000

    LuiREST / CAM-ICU * Add patient validation

--- luirest/demos/cam-icu/webclient/app/scripts/models/patient.js
+++ luirest/demos/cam-icu/webclient/app/scripts/models/patient.js
@@ -10,6 +10,30 @@ define([
     var PatientModel = Backbone.Model.extend({
         defaults: {
         },
+        validation:{
+          name: {
+             required: true
+          },
+          birthdate: {
+             required: true
+          },
+            registry: {
+                required: true
+            },
+        originationid: {
+            required: true
+        },
+            internmentdate: {
+                required: true
+            },
+            internmenttypeid: {
+                required: true
+            },
+            diagnosticid: {
+                required: true
+            }
+
+        },
       getEvaluations: function(callback){
         if (!this._evaluations){
           this._evaluations = new Evaluations({patient: this});
