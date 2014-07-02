0ff21f590cf71478aeedb3705723ad4922467e7d
luizamericop@gmail.com <luizamericop@gmail.com@d0c4a878-0f54-0410-8585-a5e1b55c9de8>
2014-07-02 20:41:37 +0000

    LuiREST / CAM-ICU * Add localstorage option

--- trunk/luirest/demos/cam-icu/webclient/app/scripts/collections/evaluation.js
+++ trunk/luirest/demos/cam-icu/webclient/app/scripts/collections/evaluation.js
@@ -6,16 +6,20 @@ define([
     'models/evaluation'
 ], function (_, Backbone, EvaluationModel) {
     'use strict';
+    
+    var props = {
+    model: EvaluationModel,
+    initialize: function(options) {
+        this.patient = options.patient;
+    },
+    url: function() {
+        return app.BASE_URL + '/patients/active/' + this.patient.get('id') + '/evaluations'
+    };
+    
+    if (app.useLocalStorage) 
+        props.localStorage = new Backbone.LocalStorage("evaluation");
 
-    var EvaluationCollection = Backbone.Collection.extend({
-        model: EvaluationModel,
-        initialize: function(options) {
-          this.patient = options.patient;
-        },
-        url: function(){
-          return app.BASE_URL + '/patients/active/' + this.patient.get('id') + '/evaluations'
-        }
-    });
+    var EvaluationCollection = Backbone.Collection.extend(props);
 
     return EvaluationCollection;
 });
