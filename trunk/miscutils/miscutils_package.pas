36be453d5f72009630e497e50e078f52df005499
luizamericop <luizamericop@d0c4a878-0f54-0410-8585-a5e1b55c9de8>
2014-07-11 00:08:30 +0000

    LuiComponents * Initial import of LuiDOMClasses

--- trunk/miscutils/miscutils_package.pas
+++ trunk/miscutils/miscutils_package.pas
@@ -1,14 +1,14 @@
 { This file was automatically created by Lazarus. Do not edit!
-This source is only used to compile and install the package.
+  This source is only used to compile and install the package.
  }
 
-unit miscutils_package; 
+unit miscutils_package;
 
 interface
 
 uses
   DomUtils, MiscUtils, DbExtensions, GnuPlotChart, SevenZipWrapper, 
-    VirtualDataObjects; 
+  VirtualDataObjects;
 
 implementation
 
