98fcfce4b6a0044cc3070e1a874c610c8e570206
luizamericop <luizamericop@d0c4a878-0f54-0410-8585-a5e1b55c9de8>
2014-11-29 16:45:04 +0000

    LuiREST / CAM-ICU * Start filtered evaluations collection

--- trunk/luirest/demos/cam-icu/lclclient/src/models/evaluationmodel.pas
+++ trunk/luirest/demos/cam-icu/lclclient/src/models/evaluationmodel.pas
@@ -5,7 +5,7 @@ unit EvaluationModel;
 interface
 
 uses
-  Classes, SysUtils, LuiJSONModel;
+  Classes, SysUtils, LuiJSONModel, fpjson;
 
 type
 
@@ -16,7 +16,65 @@ type
     class function GetResourceName: String; override;
   end;
 
+  { TEvaluations }
+
+  TEvaluations = class(TJSONCollection)
+  private
+    FFilteredData: TJSONArray;
+  protected
+    class function GetItemClass: TJSONModelClass; override;
+  public
+    constructor Create; override;
+    destructor Destroy; override;
+    procedure FilterByPatient(PatientId: Integer);
+    property FilteredData: TJSONArray read FFilteredData;
+  end;
+
 implementation
+
+uses
+  fpexprpars;
+
+{ TEvaluations }
+
+class function TEvaluations.GetItemClass: TJSONModelClass;
+begin
+  Result := TEvaluation;
+end;
+
+procedure TEvaluations.FilterByPatient(PatientId: Integer);
+var
+  Parser: TFPExpressionParser;
+  ItemData: TJSONObject;
+  i: Integer;
+begin
+  FFilteredData.Clear;
+  Parser := TFPExpressionParser.Create(nil);
+  try
+    Parser.Expression := 'patientid = ' + IntToStr(PatientId);
+    for i := 0 to Data.Count - 1 do
+    begin
+      ItemData := Data.Objects[i];
+      Parser.Identifiers.AddIntegerVariable('patientid', ItemData.Get('id', -1));
+      if (Parser.Evaluate = rtBoolean) and Parser.AsBoolean then
+        FFilteredData.Add(ItemData.Clone);
+    end;
+  finally
+    Parser.Destroy;
+  end;
+end;
+
+constructor TEvaluations.Create;
+begin
+  inherited Create;
+  FFilteredData := TJSONArray.Create;
+end;
+
+destructor TEvaluations.Destroy;
+begin
+  FFilteredData.Destroy;
+  inherited Destroy;
+end;
 
 { TEvaluation }
 
