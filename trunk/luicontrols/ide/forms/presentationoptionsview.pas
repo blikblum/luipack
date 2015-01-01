4bdc2abfc169edc5505ae25c6fef12b53c484953
luizamericop@gmail.com <luizamericop@gmail.com@d0c4a878-0f54-0410-8585-a5e1b55c9de8>
2015-01-01 20:30:10 +0000

    LuiControls / IDE * Finish implementation of Presenter/View descriptor

--- trunk/luicontrols/ide/forms/presentationoptionsview.pas
+++ trunk/luicontrols/ide/forms/presentationoptionsview.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, DividerBevel, Forms, Controls, Graphics, Dialogs,
-  StdCtrls, ExtCtrls, ValEdit, Buttons;
+  StdCtrls, ExtCtrls, ValEdit, Buttons, PresentationDescriptors;
 
 type
 
@@ -16,17 +16,27 @@ type
     CancelButton: TBitBtn;
     DividerBevel1: TDividerBevel;
     DividerBevel2: TDividerBevel;
-    Label1: TCheckBox;
+    PresenterPropertiesCheckBox: TCheckBox;
     OkButton: TBitBtn;
     PresenterClassNameEdit: TLabeledEdit;
-    ValueListEditor1: TValueListEditor;
+    PresenterPropertiesEditor: TValueListEditor;
+    UnitNameChangeTimer: TTimer;
     ViewUnitNameEdit: TLabeledEdit;
     ViewClassNameEdit: TLabeledEdit;
     PresenterUnitNameEdit: TLabeledEdit;
+    procedure FormShow(Sender: TObject);
+    procedure OkButtonClick(Sender: TObject);
+    procedure PresenterPropertiesCheckBoxChange(Sender: TObject);
+    procedure UnitNameChangeTimerTimer(Sender: TObject);
+    procedure ViewUnitNameEditChange(Sender: TObject);
   private
+    FDescriptor: TPresenterViewDescriptor;
+    FCurrentPrefix: String;
+    procedure UpdatePrefixes;
     { private declarations }
   public
     { public declarations }
+    property Descriptor: TPresenterViewDescriptor read FDescriptor write FDescriptor;
   end;
 
 var
@@ -35,6 +45,66 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TPresentationOptionsForm }
+
+procedure TPresentationOptionsForm.FormShow(Sender: TObject);
+var
+  ViewUnitName: String;
+begin
+  ViewUnitName := FDescriptor.DefaultSourceName;
+  ViewUnitNameEdit.Text := ViewUnitName;
+  ViewClassNameEdit.Text := FDescriptor.ViewClassName;
+  PresenterUnitNameEdit.Text := FDescriptor.PresenterUnitName;
+  PresenterClassNameEdit.Text := FDescriptor.PresenterClassName;
+  FCurrentPrefix := Copy(ViewUnitName, 1, Pos('view', LowerCase(ViewUnitName)) - 1);
+  ViewUnitNameEdit.OnChange := @ViewUnitNameEditChange;
+end;
+
+procedure TPresentationOptionsForm.OkButtonClick(Sender: TObject);
+begin
+  FDescriptor.DefaultSourceName := ViewUnitNameEdit.Text;
+  FDescriptor.ViewClassName := ViewClassNameEdit.Text;
+  FDescriptor.PresenterUnitName := PresenterUnitNameEdit.Text;
+  FDescriptor.PresenterClassName := PresenterClassNameEdit.Text;
+  if PresenterPropertiesCheckBox.Checked then
+    FDescriptor.PresenterProperties.Assign(PresenterPropertiesEditor.Strings);
+end;
+
+procedure TPresentationOptionsForm.PresenterPropertiesCheckBoxChange(
+  Sender: TObject);
+begin
+  PresenterPropertiesEditor.Enabled := PresenterPropertiesCheckBox.Checked;
+end;
+
+procedure TPresentationOptionsForm.UnitNameChangeTimerTimer(Sender: TObject);
+begin
+  UpdatePrefixes;
+  UnitNameChangeTimer.Enabled := False;
+end;
+
+procedure TPresentationOptionsForm.ViewUnitNameEditChange(Sender: TObject);
+begin
+  UnitNameChangeTimer.Enabled := False;
+  UnitNameChangeTimer.Enabled := True;
+end;
+
+procedure TPresentationOptionsForm.UpdatePrefixes;
+var
+  NewPrefix, ViewUnitName: String;
+  SuffixPos: Integer;
+begin
+  ViewUnitName := ViewUnitNameEdit.Text;
+  SuffixPos := Pos('view', LowerCase(ViewUnitName));
+  NewPrefix := Copy(ViewUnitNameEdit.Text, 1, SuffixPos - 1);
+  if SameText(Trim(ViewClassNameEdit.Text), Format('T%sForm', [FCurrentPrefix])) then
+    ViewClassNameEdit.Text := Format('T%sForm', [NewPrefix]);
+  if SameText(Trim(PresenterUnitNameEdit.Text), Format('%sPresenter', [FCurrentPrefix])) then
+    PresenterUnitNameEdit.Text := Format('%sPresenter', [NewPrefix]);
+  if SameText(Trim(PresenterClassNameEdit.Text), Format('T%sPresenter', [FCurrentPrefix])) then
+    PresenterClassNameEdit.Text := Format('T%sPresenter', [NewPrefix]);
+  FCurrentPrefix := NewPrefix;
+end;
 
 end.
 
