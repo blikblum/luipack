965c68b535e61397bdd60ee334084919aa4c80a3
luizamericop@gmail.com <luizamericop@gmail.com@d0c4a878-0f54-0410-8585-a5e1b55c9de8>
2015-01-01 21:02:45 +0000

    LuiComponents / IDE * Update name of classes using prefix from unit name

--- trunk/luicomponents/ide/forms/jsonmodelunitoptionsview.pas
+++ trunk/luicomponents/ide/forms/jsonmodelunitoptionsview.pas
@@ -17,12 +17,17 @@ type
     ResourceNameEdit: TLabeledEdit;
     OkButton: TBitBtn;
     CollectionNameEdit: TLabeledEdit;
+    UnitNameChangeTimer: TTimer;
     UnitNameEdit: TLabeledEdit;
     ModelNameEdit: TLabeledEdit;
     procedure OkButtonClick(Sender: TObject);
     procedure FormShow(Sender: TObject);
+    procedure UnitNameChangeTimerTimer(Sender: TObject);
+    procedure UnitNameEditChange(Sender: TObject);
   private
+    FCurrentPrefix: String;
     FDescriptor: TJSONModelUnitDescriptor;
+    procedure UpdatePrefixes;
   public
     property Descriptor: TJSONModelUnitDescriptor read FDescriptor write FDescriptor;
   end;
@@ -37,11 +42,45 @@ implementation
 { TJSONModelUnitOptionsForm }
 
 procedure TJSONModelUnitOptionsForm.FormShow(Sender: TObject);
+var
+  AUnitName: String;
 begin
   CollectionNameEdit.Text := Descriptor.CollectionName;
   ModelNameEdit.Text := Descriptor.ModelName;
   ResourceNameEdit.Text := Descriptor.ResourceName;
-  UnitNameEdit.Text := Descriptor.DefaultSourceName;
+  AUnitName := Descriptor.DefaultSourceName;
+  UnitNameEdit.Text := AUnitName;
+  FCurrentPrefix := Copy(AUnitName, 1, Pos('model', LowerCase(AUnitName)) - 1);
+  UnitNameEdit.OnChange := @UnitNameEditChange;
+end;
+
+procedure TJSONModelUnitOptionsForm.UnitNameChangeTimerTimer(Sender: TObject);
+begin
+  UpdatePrefixes;
+  UnitNameChangeTimer.Enabled := False;
+end;
+
+procedure TJSONModelUnitOptionsForm.UnitNameEditChange(Sender: TObject);
+begin
+  UnitNameChangeTimer.Enabled := False;
+  UnitNameChangeTimer.Enabled := True;
+end;
+
+procedure TJSONModelUnitOptionsForm.UpdatePrefixes;
+var
+  NewPrefix, AUnitName: String;
+  SuffixPos: Integer;
+begin
+  AUnitName := UnitNameEdit.Text;
+  SuffixPos := Pos('model', LowerCase(AUnitName));
+  NewPrefix := Copy(AUnitName, 1, SuffixPos - 1);
+  if SameText(ModelNameEdit.Text, Format('T%sModel', [FCurrentPrefix])) then
+    ModelNameEdit.Text := Format('T%sModel', [NewPrefix]);
+  if SameText(CollectionNameEdit.Text, Format('T%sCollection', [FCurrentPrefix])) then
+    CollectionNameEdit.Text := Format('T%sCollection', [NewPrefix]);
+  if SameText(ResourceNameEdit.Text, FCurrentPrefix) then
+    ResourceNameEdit.Text := LowerCase(NewPrefix);
+  FCurrentPrefix := NewPrefix;
 end;
 
 procedure TJSONModelUnitOptionsForm.OkButtonClick(Sender: TObject);
