f494bdce6a0d76cdcfb4e920044ac178a93e5a8e
luizamericop <luizamericop@gmail.com>
2015-06-18 00:46:56 +0000

    LuiREST / Sqldb + Implement TSqldbCollectionResource + Add demo

---
+++ luirest/demos/addressbook/sqldbcollection/addressbookresources.pas
@@ -0,0 +1,119 @@
+unit AddressBookResources;
+
+{$mode objfpc}{$H+}
+{$define USE_SQLITE3_SLIM}
+
+interface
+
+uses
+  Classes, SysUtils, LuiRESTServer, LuiRESTSqldb, sqldb, HTTPDefs,
+  {$ifdef USE_SQLITE3_SLIM}
+  sqlite3slimconn
+  {$else}
+  sqlite3conn
+  {$endif};
+
+type
+
+  { TServiceInfoResource }
+
+  TServiceInfoResource = class(TRESTResource)
+  public
+    procedure HandleGet(ARequest: TRequest; AResponse: TResponse); override;
+  end;
+
+  { TCategoriesResource }
+
+  TCategoriesResource = class(TSqldbCollectionResource)
+  protected
+    class function GetItemParam: String; override;
+  public
+    constructor Create; override;
+  end;
+
+  { TContactsResource }
+
+  TContactsResource = class(TSqldbCollectionResource)
+  protected
+    class function GetItemParam: String; override;
+    procedure PrepareItem(ItemResource: TSqldbJSONResource); override;
+  public
+    constructor Create; override;
+  end;
+
+  { TContactPhonesResource }
+
+  TContactPhonesResource = class(TSqldbCollectionResource)
+  protected
+    class function GetItemParam: String; override;
+    procedure PrepareItem(ItemResource: TSqldbJSONResource); override;
+  public
+    constructor Create; override;
+  end;
+
+implementation
+
+{ TContactPhonesResource }
+
+class function TContactPhonesResource.GetItemParam: String;
+begin
+  Result := 'phoneid';
+end;
+
+procedure TContactPhonesResource.PrepareItem(ItemResource: TSqldbJSONResource);
+begin
+  inherited PrepareItem(ItemResource);
+  ItemResource.InputFields := '["number"]';
+end;
+
+constructor TContactPhonesResource.Create;
+begin
+  inherited Create;
+  SelectSQL := 'Select Id, ContactId, Number from Phones';
+  ConditionsSQL := 'where ContactId = :contactid';
+  OutputFields := '["id", "number"]';
+  InputFields := '["number", "contactid"]';
+end;
+
+{ TCategoriesResource }
+
+class function TCategoriesResource.GetItemParam: String;
+begin
+  Result := 'categoryid';
+end;
+
+constructor TCategoriesResource.Create;
+begin
+  inherited Create;
+  SelectSQL := 'Select Id, Name from Categories';
+end;
+
+{ TContactsResource }
+
+class function TContactsResource.GetItemParam: String;
+begin
+  Result := 'contactid';
+end;
+
+procedure TContactsResource.PrepareItem(ItemResource: TSqldbJSONResource);
+begin
+  inherited PrepareItem(ItemResource);
+  ItemResource.RegisterSubPath('phones', TContactPhonesResource, 0);
+end;
+
+constructor TContactsResource.Create;
+begin
+  inherited Create;
+  SelectSQL := 'Select Id, CategoryId, Name from contacts';
+end;
+
+{ TServiceInfoResource }
+
+procedure TServiceInfoResource.HandleGet(ARequest: TRequest;
+  AResponse: TResponse);
+begin
+  AResponse.Contents.Add('{"version":"0.1"}');
+end;
+
+end.
+
---
+++ luirest/demos/addressbook/sqldbcollection/mainservice.pas
@@ -0,0 +1,62 @@
+unit MainService;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, fphttp, HTTPDefs, LuiRESTServer, sqlite3slimconn,
+  sqldb, AddressBookResources;
+
+type
+
+  { TMainServiceModule }
+
+  TMainServiceModule = class(TRESTServiceModule)
+    SQLConnector: TSQLConnector;
+    SQLTransaction: TSQLTransaction;
+  private
+    procedure InitConnection;
+  public
+    constructor Create(AOwner: TComponent); override;
+  end;
+
+var
+  MainServiceModule: TMainServiceModule;
+
+implementation
+
+uses
+  LuiRESTSqldb, IniFiles;
+
+{$R *.lfm}
+
+{ TMainServiceModule }
+
+procedure TMainServiceModule.InitConnection;
+var
+  Config: TIniFile;
+  DatabaseType: String;
+begin
+  Config := TIniFile.Create('addressbookdb.ini');
+  DatabaseType := Config.ReadString('database', 'type', 'sqlite3slim');
+  SQLConnector.ConnectorType := DatabaseType;
+  SQLConnector.DatabaseName := Config.ReadString(DatabaseType, 'databasename', 'addressbookdata.db');
+  SQLConnector.HostName := Config.ReadString(DatabaseType, 'hostname', 'localhost');
+  SQLConnector.Password := Config.ReadString(DatabaseType, 'password', '');
+  SQLConnector.UserName := Config.ReadString(DatabaseType, 'username', '');
+  Config.Destroy;
+end;
+
+constructor TMainServiceModule.Create(AOwner: TComponent);
+begin
+  inherited Create(AOwner);
+  InitConnection;
+  TSqldbJSONResource.DefaultConnection := SQLConnector;
+  Resources.Register('contacts', TContactsResource, 0);
+  Resources.Register('categories', TCategoriesResource, 0);
+  Resources.Register('info', TServiceInfoResource, 0);
+end;
+
+end.
+
