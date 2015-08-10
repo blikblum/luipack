bc358be268adef432d6191af645e8598ad6c3b87
luizamericop@gmail.com <luizamericop@gmail.com@d0c4a878-0f54-0410-8585-a5e1b55c9de8>
2015-08-10 00:10:38 +0000

    DataHub * Initial implementation of TField.Constraints

--- trunk/datahub/app/src/presentations/modelfieldeditor.pas
+++ trunk/datahub/app/src/presentations/modelfieldeditor.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, RTTICtrls, Forms, Controls, Graphics, Dialogs,
-  Buttons, StdCtrls, DataModel;
+  Buttons, StdCtrls, DataModel, VTJSON, fpjson, VirtualTrees;
 
 type
 
@@ -18,15 +18,30 @@ type
     Label1: TLabel;
     Label2: TLabel;
     Label3: TLabel;
+    Label4: TLabel;
     SaveButton: TBitBtn;
     FieldNameEdit: TTIEdit;
     FieldTypeComboBox: TTIComboBox;
-    procedure FormCreate(Sender: TObject);
-    procedure FormDestroy(Sender: TObject);
+    ConstraintsInspector: TVirtualJSONInspector;
+    procedure ConstraintsInspectorAfterCellPaint(Sender: TBaseVirtualTree;
+      TargetCanvas: TCanvas; Node: PVirtualNode; Column: TColumnIndex;
+      const CellRect: TRect);
+    procedure ConstraintsInspectorClick(Sender: TObject);
+    procedure ConstraintsInspectorFilterProperty(Sender: TVirtualJSONInspector;
+      ParentData, ItemData: TJSONData; ItemIndex: Integer; var Accept: Boolean);
+    procedure ConstraintsInspectorFormatValue(Sender: TVirtualJSONInspector;
+      const PropName: String; PropData: TJSONData; var DisplayText: String);
+    procedure FieldTypeComboBoxEditingDone(Sender: TObject);
     procedure FormShow(Sender: TObject);
+    procedure SaveButtonClick(Sender: TObject);
   private
     FEditField: TDataModelField;
+    FEditConstraintsData: TJSONObject;
+    procedure LoadConstraintsData;
+    procedure SaveConstraintsData;
   public
+    constructor Create(TheOwner: TComponent); override;
+    destructor Destroy; override;
     class function EditField(Field: TDataModelField): Boolean;
     class function AddField(Fields: TDataModelFields): Boolean;
   end;
@@ -37,25 +52,164 @@ var
 implementation
 
 uses
-  DataHubUtils;
+  DataHubUtils, LuiJSONUtils, strutils, Themes, TmSchema;
 
 {$R *.lfm}
 
 { TModelFieldEditorForm }
 
-procedure TModelFieldEditorForm.FormCreate(Sender: TObject);
+const
+  NumberRules: array[0..1] of String = ('minimum', 'maximum');
+  StringRules: array[0..2] of String = ('pattern', 'minLength', 'maxLength');
+
+type
+  TStringArray = array of String;
+
+function GetRulesForFieldType(FieldType: TDataModelFieldType): TStringArray;
 begin
-  FEditField := TDataModelField.Create(nil);
+  Result := nil;
+  case FieldType of
+     dftString, dftMemo:
+       Result := StringRules;
+     dftInteger, dftFloat, dftCurrency:
+       Result := NumberRules;
+   end;
+   SetLength(Result, Length(Result) + 1);
+   Result[Length(Result) - 1] := 'required';
 end;
 
-procedure TModelFieldEditorForm.FormDestroy(Sender: TObject);
+procedure TModelFieldEditorForm.ConstraintsInspectorFilterProperty(
+  Sender: TVirtualJSONInspector; ParentData, ItemData: TJSONData;
+  ItemIndex: Integer; var Accept: Boolean);
+var
+  PropName: String;
 begin
-  FEditField.Destroy;
+  if ParentData.JSONType <> jtObject then
+    Exit;
+  PropName := TJSONObject(ParentData).Names[ItemIndex];
+  Accept := AnsiMatchStr(PropName, GetRulesForFieldType(FEditField.FieldType));
+end;
+
+procedure TModelFieldEditorForm.ConstraintsInspectorFormatValue(
+  Sender: TVirtualJSONInspector; const PropName: String; PropData: TJSONData;
+  var DisplayText: String);
+begin
+  if PropData.JSONType in [jtBoolean, jtNull] then
+    DisplayText := '';
+end;
+
+procedure TModelFieldEditorForm.ConstraintsInspectorAfterCellPaint(
+  Sender: TBaseVirtualTree; TargetCanvas: TCanvas; Node: PVirtualNode;
+  Column: TColumnIndex; const CellRect: TRect);
+var
+  PropData: TJSONData;
+  PropName: String;
+  Details: TThemedElementDetails;
+  R: TRect;
+begin
+  if Column = 1 then
+  begin
+    PropData := ConstraintsInspector.GetData(Node);
+    PropName := ConstraintsInspector.GetName(Node);
+    if PropName = 'required' then
+    begin
+      Details.Element := teButton;
+      Details.Part := BP_CHECKBOX;
+      R := Rect(CellRect.Left + 10, CellRect.Top + 2, CellRect.Left + 26, CellRect.Top + 18);
+      if PropData is TJSONBoolean then
+      begin
+        if PropData.AsBoolean then
+          Details.State := 5
+        else
+          Details.State := 1;
+      end
+      else
+        Details.State := 4;
+      ThemeServices.DrawElement(TargetCanvas.Handle, Details, R);
+    end;
+  end;
+end;
+
+procedure TModelFieldEditorForm.ConstraintsInspectorClick(Sender: TObject);
+var
+  P: TPoint;
+  PropData: TJSONData;
+  Info: THitInfo;
+begin
+  P := ConstraintsInspector.ScreenToClient(Mouse.CursorPos);
+  ConstraintsInspector.GetHitTestInfoAt(P.x, P.y, True, Info);
+  if (Info.HitNode <> nil) and (Info.HitColumn = 1) then
+  begin
+    if ConstraintsInspector.GetName(Info.HitNode) = 'required' then
+    begin
+      PropData := ConstraintsInspector.GetData(Info.HitNode);
+      if not (PropData is TJSONBoolean) or not PropData.AsBoolean then
+        FEditConstraintsData.Booleans['required'] := True
+      else
+        FEditConstraintsData.Booleans['required'] := False;
+      ConstraintsInspector.Reload;
+    end;
+  end;
+end;
+
+procedure TModelFieldEditorForm.FieldTypeComboBoxEditingDone(Sender: TObject);
+begin
+  ConstraintsInspector.Reload;
+  ConstraintsInspector.Invalidate; //todo: should not be necessary Investigate
 end;
 
 procedure TModelFieldEditorForm.FormShow(Sender: TObject);
 begin
   SetRTTILinkObject(Self, FEditField);
+  LoadConstraintsData;
+end;
+
+procedure TModelFieldEditorForm.SaveButtonClick(Sender: TObject);
+begin
+  SaveConstraintsData;
+end;
+
+procedure TModelFieldEditorForm.LoadConstraintsData;
+begin
+  if FEditField.ConstraintsData <> nil then
+    CopyJSONObject(FEditField.ConstraintsData, FEditConstraintsData);
+  ConstraintsInspector.RootData := FEditConstraintsData;
+end;
+
+procedure TModelFieldEditorForm.SaveConstraintsData;
+var
+  TmpData: TJSONObject;
+begin
+  TmpData := TJSONObject.Create;
+  try
+    CopyJSONObject(FEditConstraintsData, TmpData, GetRulesForFieldType(FEditField.FieldType));
+    FEditField.Constraints := TmpData.AsJSON;
+  finally
+    TmpData.Destroy;
+  end;
+end;
+
+constructor TModelFieldEditorForm.Create(TheOwner: TComponent);
+begin
+  inherited Create(TheOwner);
+  FEditField := TDataModelField.Create(nil);
+  FEditConstraintsData := TJSONObject.Create(
+    [
+    'required', nil,
+    'minimum', nil,
+    'maximum', nil,
+    'minLength', nil,
+    'maxLength', nil,
+    'pattern', nil
+    ]
+  );
+end;
+
+destructor TModelFieldEditorForm.Destroy;
+begin
+  FEditField.Destroy;
+  FEditConstraintsData.Destroy;
+  inherited Destroy;
 end;
 
 class function TModelFieldEditorForm.EditField(Field: TDataModelField): Boolean;
