c8a576ab307cd73d33cd5fca228d9cb195207063
luizamericop@gmail.com <luizamericop@gmail.com@d0c4a878-0f54-0410-8585-a5e1b55c9de8>
2015-08-15 10:43:20 +0000

    LuiREST / demos + Use sqlite3slimconnection in LogService

--- trunk/luirest/demos/eventlog/service/mainservice.pas
+++ trunk/luirest/demos/eventlog/service/mainservice.pas
@@ -5,7 +5,7 @@ unit MainService;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, fphttp, HTTPDefs, LuiRESTServer, sqlite3conn,
+  Classes, SysUtils, FileUtil, fphttp, HTTPDefs, LuiRESTServer, sqlite3slimconn,
   sqldb, LuiRESTSqldb;
 
 type
