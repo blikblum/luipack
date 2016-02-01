6359b53867bacb32f4d2abda7bd868a3bb45a1cd
Luiz Américo <luizamericop@gmail.com>
2016-01-31 22:40:34 -0300

    JSONUtils: cleanup tests

--- luicomponents/tests/jsonutilstests.pas
+++ luicomponents/tests/jsonutilstests.pas
@@ -12,10 +12,6 @@ type
   { TCompareDataTestCase }
 
   TCompareDataTestCase = class(TTestCase)
-  private
-
-  protected
-    procedure TearDown; override;
   published
     procedure CompareNull;
     procedure CompareInteger;
@@ -27,23 +23,18 @@ type
 implementation
 
 uses
-  LuiJSONUtils, Math;
-
-procedure TCompareDataTestCase.TearDown;
-begin
-  inherited TearDown;
-end;
+  LuiJSONUtils;
 
 procedure TCompareDataTestCase.CompareNull;
 begin
-  CheckEquals(0, CompareJSONData(TJSONNull.Create, TJSONNull.Create), 'Null shoud be equal to null');
+  CheckEquals(0, CompareJSONData(TJSONNull.Create, TJSONNull.Create), 'Null should be equal to null');
 end;
 
 procedure TCompareDataTestCase.CompareInteger;
 begin
-  CheckEquals(0, CompareJSONData(TJSONIntegerNumber.Create(1), TJSONIntegerNumber.Create(1)), '1 shoud be equal to 1');
-  CheckEquals(-1, CompareJSONData(TJSONIntegerNumber.Create(1), TJSONIntegerNumber.Create(2)), '1 shoud be less then 2');
-  CheckEquals(1, CompareJSONData(TJSONIntegerNumber.Create(2), TJSONIntegerNumber.Create(1)), '2 shoud be less then 1');
+  CheckEquals(0, CompareJSONData(TJSONIntegerNumber.Create(1), TJSONIntegerNumber.Create(1)), '1 should be equal to 1');
+  CheckEquals(-1, CompareJSONData(TJSONIntegerNumber.Create(1), TJSONIntegerNumber.Create(2)), '1 should be less then 2');
+  CheckEquals(1, CompareJSONData(TJSONIntegerNumber.Create(2), TJSONIntegerNumber.Create(1)), '2 should be less then 1');
 end;
 
 procedure TCompareDataTestCase.CompareObject;
