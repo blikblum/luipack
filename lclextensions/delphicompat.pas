a14bd6eb156fcbdd3e69c6ecc70bc1c88ad9216b
Luiz Américo <brandkarl@gmal.com>
2016-07-17 22:04:20 -0300

    delphicompat: fix compilation with newer LCL

--- lclextensions/delphicompat.pas
+++ lclextensions/delphicompat.pas
@@ -38,7 +38,7 @@ unit DelphiCompat;
 interface
 
 uses
-  LMessages, Types, Classes,  LCLType, LCLVersion;
+  LMessages, Types, Classes, LCLType, LCLVersion;
 
 const
   //Messages
@@ -135,7 +135,7 @@ implementation
 
 uses
 {$i uses.inc}
-  maps, LCLProc, LCLMessageGlue, Controls
+  maps, LazUTF8, LCLMessageGlue, Controls
   {$ifdef DEBUG_DELPHICOMPAT}
   ,multiloglcl, filechannel
   {$endif}
