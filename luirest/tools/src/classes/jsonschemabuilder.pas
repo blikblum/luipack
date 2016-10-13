2859f565cae0ca521c79e614e5b79d1e70f80241
Luiz Américo <luizamericop@gmail.com>
2016-10-12 21:55:23 -0300

    RESTAPIBuilder: Implement generating array schema

--- luirest/tools/src/classes/jsonschemabuilder.pas
+++ luirest/tools/src/classes/jsonschemabuilder.pas
@@ -33,6 +33,9 @@ function CreateJSONSchema(Data: TJSONData): TJSONObject;
 
 implementation
 
+uses
+  LuiJSONHelpers, LuiJSONUtils;
+
 function CreateJSONSchema(Data: TJSONData): TJSONObject;
 var
   Builder: TJSONSchemaBuilder;
@@ -100,8 +103,60 @@ begin
 end;
 
 procedure TJSONSchemaBuilder.CreateArrayDefinition(Data: TJSONArray; Result: TJSONObject);
+var
+  ItemsSchemaData, PropertiesData, ItemData, SampleData: TJSONObject;
+  ItemPropData: TJSONData;
+  NullProps: TStringList;
+  i, k: Integer;
+  PropName, PropType: String;
 begin
-  //todo
+  ItemsSchemaData := TJSONObject.Create;
+  Result.Strings['type'] := 'array';
+  Result.Objects['items'] := ItemsSchemaData;
+
+  //check each item format
+  //todo: improve heuristics
+  NullProps := TStringList.Create;
+  NullProps.Duplicates := dupIgnore;
+  SampleData := TJSONObject.Create;
+  try
+    for i := 0 to Data.Count - 1 do
+    begin
+      ItemData := Data.Objects[i];
+      for k := 0 to ItemData.Count - 1 do
+      begin
+        ItemPropData := ItemData.Items[k];
+        PropName := ItemData.Names[k];
+        if ItemPropData.JSONType = jtNull then
+          NullProps.Add(PropName)
+        else
+        begin
+          if SampleData.IndexOfName(PropName) = -1  then
+            SampleData.Add(PropName, ItemPropData.Clone);
+        end;
+      end;
+    end;
+    for i := 0 to NullProps.Count - 1 do
+    begin
+      PropName := NullProps[i];
+      if SampleData.IndexOfName(PropName) = -1 then
+        SampleData.Nulls[PropName] := True;
+    end;
+    CreateObjectDefinition(SampleData, ItemsSchemaData);
+    if ItemsSchemaData.Find('properties', PropertiesData) then
+    begin
+      for i := 0 to NullProps.Count - 1 do
+      begin
+        PropName := NullProps[i];
+        PropType := PropertiesData.GetPath(PropName + '.type', 'null');
+        if PropType <> 'null' then
+          SetJSONPath(PropertiesData, PropName + '.type', TJSONArray.Create([PropType, nil]));
+      end;
+    end;
+  finally
+    NullProps.Destroy;
+    SampleData.Destroy;
+  end;
 end;
 
 procedure TJSONSchemaBuilder.CreateObjectDefinition(Data: TJSONObject; Result: TJSONObject);
