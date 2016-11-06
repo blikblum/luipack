403fd0c047cafe98e2622a09cb4ae3fe674c2c69
Luiz Américo <brandkarl@gmail.com>
2016-11-05 21:14:49 -0300

    LCLExtensions: fix compilation with fpc 3.0.1

--- lclextensions/oleutils.pas
+++ lclextensions/oleutils.pas
@@ -104,7 +104,7 @@ begin
   //soFrom* constants are equal to STREAM_SEEK_* constants. Assume it here
   liOffset.LowPart:=Offset;
   liOffset.HighPart:=0;
-  {$if FPC_FULLVERSION >= 30100}
+  {$if FPC_FULLVERSION >= 30001}
   Res:=FSrcStream.Seek(QWord(liOffset), Origin, QWord(liResult));
   {$else}
   Res:=FSrcStream.Seek(Int64(liOffset), Origin, Int64(liResult));
