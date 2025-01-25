77eb9b40f33bb240f3b221fe99c06632874312ff
Artem V. Ageev <artem.v.ageev@gmail.com>
2025-01-25 13:24:55 +0200

    add fptest

--- .github/workflows/make.pas
+++ .github/workflows/make.pas
@@ -213,7 +213,7 @@ begin
   try
     if ParamCount > 0 then
       case ParamStr(1) of
-        'build': BuildAll([]);
+        'build': BuildAll(['MultiLog', 'VampyreImaging']);
         else
           OutLog(etError, ParamStr(1));
       end;
